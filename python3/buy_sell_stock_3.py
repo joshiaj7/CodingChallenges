fdc51691f0e38cd0401d003f7b245418002dc88b
joshiaj7 <joshia.tan7@gmail.com>
2020-08-16 21:40:48 +0700

    leetcode 2020-8-16

---
+++ python3/buy_sell_stock_3.py
@@ -0,0 +1,15 @@
+# leetcode
+
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        firstBuy, secondBuy = (10**10) * -1, (10**10) * -1
+        firstSold, secondSold = 0, 0
+        
+        for i in range(len(prices)):
+            firstBuy = max(firstBuy, -prices[i])
+            firstSold = max(firstSold, firstBuy + prices[i])
+            secondBuy = max(secondBuy, firstSold - prices[i])
+            secondSold = max(secondSold, secondBuy + prices[i])
+            # print(firstBuy, firstSold, secondBuy, secondSold)
+        
+        return secondSold

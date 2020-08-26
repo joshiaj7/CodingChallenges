79f9e89d7dced2188af921188d20b68c5b5c3863
joshiaj7 <joshia.tan7@gmail.com>
2020-08-26 14:25:05 +0700

    leetcode 2020-8-26

---
+++ golang/fizzbuzz.go
@@ -0,0 +1,25 @@
+import (
+    "fmt"
+    "strconv"
+)
+
+func fizzBuzz(n int) []string {
+    var ans []string
+    var item string
+    
+    for i := 0; i < n; i++ {
+        item = ""
+        if (i+1)%3 == 0 {
+            item += "Fizz"
+        } 
+        if (i+1)%5 == 0 {
+            item += "Buzz"
+        }
+        if item == "" {
+            item = strconv.Itoa(i+1)
+        }
+        ans = append(ans, item)
+    }
+    
+    return ans
+}

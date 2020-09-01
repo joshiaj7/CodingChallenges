3f0aa4b27a0193a608090a74beeee7353d4d4c2c
joshiaj7 <joshia.tan7@gmail.com>
2020-09-01 16:36:01 +0700

    leetcode 2020-9-1

---
+++ python3/largest_time_for_given_digits.py
@@ -0,0 +1,16 @@
+from itertools import permutations 
+
+class Solution:
+    def largestTimeFromDigits(self, A: List[int]) -> str:
+        ans = ""
+        perms = permutations(A) 
+
+        # print(perms)
+        for i in list(perms): 
+            if (((i[0] == 2 and i[1] <= 3) or i[0] < 2) and i[2] < 6):
+                temp = "{}{}:{}{}".format(i[0], i[1], i[2], i[3])
+                if ans < temp:
+                    ans = temp
+
+        return ans
+        

a12d0c277cc6a4d2273cde7ab9018c0c195dd229
joshiaj7 <joshia.tan7@gmail.com>
2020-09-02 21:16:43 +0700

    leetcode 2020-9-2

---
+++ python3/contains_duplicate_3.py
@@ -0,0 +1,13 @@
+# leetcode
+
+class Solution:
+    def containsNearbyAlmostDuplicate(self, nums: List[int], k: int, t: int) -> bool:
+        if t == 0 and len(nums) == len(set(nums)):
+            return False
+
+        for i, cur_val in enumerate(nums):
+            for j in range(i+1, min(i+k+1, len(nums))):
+                if abs(cur_val - nums[j]) <= t:
+                    return True
+
+        return False

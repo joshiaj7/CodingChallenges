53ca37233f2c7295a491f486230e556f81715780
joshiaj7 <joshia.tan7@gmail.com>
2020-09-12 18:42:47 +0700

    leetcode 2020-9-12

---
+++ python3/combination_sum_3.py
@@ -0,0 +1,13 @@
+from itertools import combinations
+
+
+class Solution:
+    def combinationSum3(self, k: int, n: int) -> List[List[int]]:
+        ans = []
+
+        for item in combinations(range(1, 10), k):
+            temp = list(item)
+            if sum(temp) == n:
+                ans.append(temp)
+
+        return ans

5681448135eb939a1f74886dab99aaa7af6311a5
joshiaj7 <joshia.tan7@gmail.com>
2020-09-14 15:25:15 +0700

    leetcode 2020-9-14

---
+++ golang/house_robber.go
@@ -0,0 +1,26 @@
+func rob(nums []int) int {
+    ans := 0
+    leng := len(nums)
+    dp := make([]int, leng)
+    
+    for i, v := range(nums) {
+        if i == 0 || i == 1 {
+            dp[i] = v
+        } else if i == 2 {
+            dp[i] = v + dp[i-2]
+        } else {
+            if dp[i-2] > dp[i-3] {
+                dp[i] = v + dp[i-2]
+            } else {
+                dp[i] = v + dp[i-3]
+            }
+        }
+        
+        if ans < dp[i] {
+            ans = dp[i]
+        }
+        
+    }
+    
+    return ans
+}

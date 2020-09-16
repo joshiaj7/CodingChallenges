1ef5904e22796d90e2723fff3b4b7204382df712
joshiaj7 <joshia.tan7@gmail.com>
2020-09-16 16:26:43 +0700

    leetcode 2020-9-16

---
+++ golang/maximum_xor_of_two_num.go
@@ -0,0 +1,13 @@
+func findMaximumXOR(nums []int) int {
+    ans := 0
+    
+    for i := 0; i < len(nums); i++ {
+        for j := i; j < len(nums); j++ {
+            if ans < nums[i] ^ nums[j] {
+                ans = nums[i] ^ nums[j]
+            }
+        }
+    }
+    
+    return ans
+}

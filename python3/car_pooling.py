9a2a7de1e09cf1b88a2e8b0cc090842129543868
joshiaj7 <joshia.tan7@gmail.com>
2020-09-21 14:20:44 +0700

    leetcode 2020-9-21

---
+++ python3/car_pooling.py
@@ -0,0 +1,15 @@
+class Solution:
+    def carPooling(self, trips: List[List[int]], capacity: int) -> bool:
+        dp = [0] * 1001
+        pass_req = 0
+
+        for trip in trips:
+            dp[trip[1]] += trip[0]
+            dp[trip[2]] -= trip[0]
+
+        for i in dp:
+            pass_req += i
+            if pass_req > capacity:
+                return False
+
+        return True

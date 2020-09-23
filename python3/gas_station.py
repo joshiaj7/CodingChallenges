2887f04bc270b8d23835195f16e638f6c5ba4ea6
joshiaj7 <joshia.tan7@gmail.com>
2020-09-23 15:18:40 +0700

    leetcode 2020-9-23

---
+++ python3/gas_station.py
@@ -0,0 +1,25 @@
+class Solution:
+    def canCompleteCircuit(self, gas: List[int], cost: List[int]) -> int:
+        N = len(gas)
+        dp = []
+
+        for i in range(N):
+            dp.append(gas[i] - cost[i])
+
+        print(dp)
+
+        if sum(dp) < 0:
+            return -1
+
+        temp = 0
+        ans = -1
+
+        for idx in range(N):
+            temp += dp[idx]
+            if temp >= 0 and ans == -1:
+                ans = idx
+            if temp < 0:
+                ans = -1
+            temp = max(temp, 0)
+
+        return ans

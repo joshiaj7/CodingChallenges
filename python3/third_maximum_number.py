a1041013001dce352c58f30776fb9c12c061f66d
joshiaj7 <joshia.tan7@gmail.com>
2020-10-06 16:35:12 +0700

    daily leetcode challenge

---
+++ python3/third_maximum_number.py
@@ -0,0 +1,27 @@
+"""
+Space   : O(1)
+Time    : O(n)
+"""
+
+
+class Solution:
+    def thirdMax(self, nums: List[int]) -> int:
+        a, b, c = -10**10, -10**10, -10**10
+
+        nums = list(set(nums))
+
+        for i in nums:
+            if i > a:
+                c = b
+                b = a
+                a = i
+            elif i > b:
+                c = b
+                b = i
+            elif i > c:
+                c = i
+            # print(a, b, c)
+
+        if c == -10**10:
+            return a
+        return c

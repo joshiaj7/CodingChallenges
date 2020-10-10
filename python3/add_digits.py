5a41a3c378a966305eb391ea6c138a5e10fa9c7f
joshiaj7 <joshia.tan7@gmail.com>
2020-10-10 15:17:40 +0700

    daily leetcode challenge

---
+++ python3/add_digits.py
@@ -0,0 +1,21 @@
+"""
+Space   : O(1)
+Time    : O(n)
+"""
+
+
+class Solution:
+    def addDigits(self, num: int) -> int:
+        ans = 0
+
+        if num < 10:
+            return num
+
+        while len(str(num)) > 1:
+            n = str(num)
+            temp = 0
+            for i in n:
+                temp += int(i)
+            num = temp
+
+        return num

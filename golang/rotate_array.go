7c4df94ebd45fcfceb4fb8af575bb3dcd0139748
joshiaj7 <joshia.tan7@gmail.com>
2020-10-15 14:48:16 +0700

    leetcode 2020-10-15

---
+++ golang/rotate_array.go
@@ -0,0 +1,23 @@
+package golang
+
+// Space	: O(k)
+// Time	: O(n)
+
+func rotate(nums []int, k int)  {
+    n := len(nums)
+    k %= n
+    var temp []int
+    
+    for i := 0; i < k; i++ {
+        temp = append(temp, nums[n-k+i])
+    }
+    
+    for j := n-1; j >= 0; j-- {
+        if j >= k {
+            nums[j] = nums[j-k]
+        } else {
+            nums[j] = temp[j]
+        }
+        
+    }
+}

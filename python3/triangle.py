79f239ded01eca5a8f2396a1953d931c30748782
joshiaj7 <joshia.tan7@gmail.com>
2020-10-20 15:47:03 +0700

    daily leetcode challenge

---
+++ python3/triangle.py
@@ -0,0 +1,30 @@
+"""
+Space   : O(1)
+Time    : O(n)
+"""
+
+
+class Solution:
+    def minimumTotal(self, triangle: List[List[int]]) -> int:
+        ans = 0
+        n = len(triangle)
+
+        if n == 1:
+            return triangle[0][0]
+
+        ans = 10**10
+        for i in range(1, n):
+            item = triangle[i]
+            m = len(item)
+            for j in range(m):
+                if j == 0:
+                    triangle[i][j] += triangle[i-1][0]
+                elif j == m-1:
+                    triangle[i][j] += triangle[i-1][j-1]
+                else:
+                    triangle[i][j] += min(triangle[i-1][j-1], triangle[i-1][j])
+
+                if i == n-1:
+                    ans = min(ans, triangle[i][j])
+
+        return ans

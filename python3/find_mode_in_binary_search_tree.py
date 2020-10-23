314e31dca888be61f87dee7203f3a551730ef3d1
joshiaj7 <joshia.tan7@gmail.com>
2020-10-23 22:03:03 +0700

    daily leetcode challenge

---
+++ python3/find_mode_in_binary_search_tree.py
@@ -0,0 +1,46 @@
+"""
+Space   : O(n)
+Time    : O(n)
+"""
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+
+class Solution:
+    def findMode(self, root: TreeNode) -> List[int]:
+        if not root:
+            return[]
+
+        mem = []
+
+        stack = [root]
+
+        while stack:
+            node = stack.pop()
+            mem.append(node.val)
+            if node.left:
+                stack.append(node.left)
+            if node.right:
+                stack.append(node.right)
+
+        hashmap = {}
+        max_count = 0
+
+        for i in mem:
+            if i not in hashmap:
+                hashmap[i] = 1
+            else:
+                hashmap[i] += 1
+            max_count = max(max_count, hashmap[i])
+
+        mode = []
+        for k, v in hashmap.items():
+            if v == max_count:
+                mode.append(k)
+
+        return mode

ba5efa0be6f7b30c591f90f5b39cb42d7f3d3d2d
joshiaj7 <joshia.tan7@gmail.com>
2020-10-25 20:46:13 +0700

    daily leetcode challenges

---
+++ python3/minimum_index_sum_of_two_lists.py
@@ -0,0 +1,33 @@
+"""
+Space   : O(n)
+Time    : O(n)
+"""
+
+
+class Solution:
+    def findRestaurant(self, list1: List[str], list2: List[str]) -> List[str]:
+        h1, h2 = {}, {}
+
+        for i in range(len(list1)):
+            h1[list1[i]] = i
+
+        for j in range(len(list2)):
+            h2[list2[j]] = j
+
+        # print(h1, h2)
+
+        min_idx = 10**10
+        ans = []
+        intercept = {}
+
+        for k, v in h1.items():
+            if k in h2:
+                intercept[k] = v + h2[k]
+                min_idx = min(min_idx, intercept[k])
+
+        for key, val in intercept.items():
+            if val == min_idx:
+                ans.append(key)
+
+        # print(min_idx)
+        return ans

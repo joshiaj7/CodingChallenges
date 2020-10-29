d669debf6c2ee67f7856889824ac8916c37f69d4
joshiaj7 <joshia.tan7@gmail.com>
2020-10-29 15:38:09 +0700

    leetcode 2020-10-29

---
+++ golang/maximize_distance_to_closest_person.go
@@ -0,0 +1,44 @@
+package golang
+
+func max(a int, b int) int {
+    if a > b {
+        return a
+    }
+    return b
+}
+
+
+func maxDistToClosest(seats []int) int {
+    var forward, backward, dist, mid, ans int
+    
+    n := len(seats)
+    
+    for k, v := range(seats) {
+        // forward
+        if v == 0 {
+            forward++
+            dist = max(dist, forward)
+        } else {
+            forward = 0
+        }
+        
+        // backward
+        if seats[n-1-k] == 0 {
+            backward++
+        } else {
+            backward = 0
+        }
+        
+    }
+    
+    if dist % 2 == 0 {
+        mid = dist / 2
+    } else {
+        mid = (dist + 1) / 2
+    }
+
+    ans = max(ans, mid)
+    ans = max(ans, forward)
+    ans = max(ans, backward)
+    return ans
+}

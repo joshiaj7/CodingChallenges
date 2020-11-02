9fd1af4f38111d8e292531ab6a303fd484ae3615
joshiaj7 <joshia.tan7@gmail.com>
2020-11-02 16:48:44 +0700

    leetcode 2020-11-2

---
+++ python3/insertion_sort_list.py
@@ -0,0 +1,46 @@
+"""
+Space   : O(n)
+Time    : O(n^2)
+"""
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+
+
+class Solution:
+    def insertionSortList(self, head: ListNode) -> ListNode:
+        if not head:
+            return head
+
+        mem = []
+
+        point = head
+        while point:
+            mem.append(point.val)
+            point = point.next
+
+        n = len(mem)
+
+        stack = []
+        for i in range(n):
+            stack.append(mem[i])
+            j = len(stack) - 1
+            while j > 0:
+                if stack[j] < stack[j-1]:
+                    temp = stack[j-1]
+                    stack[j-1] = stack[j]
+                    stack[j] = temp
+                else:
+                    break
+                j -= 1
+
+        ans = ListNode(stack[0])
+        p = ans
+        for i in range(1, len(stack)):
+            p.next = ListNode(stack[i])
+            p = p.next
+
+        return ans

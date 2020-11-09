4de6448a2fd8e611cb973d1ac94e4b760d367558
joshiaj7 <joshia.tan7@gmail.com>
2020-11-09 15:23:43 +0700

    leetcode 2020-11-9

---
+++ python3/maximum_difference_between_node_and_ancestor.py
@@ -0,0 +1,33 @@
+"""
+Space   : O(n)
+Time    : O(n)
+"""
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+
+class Solution:
+    def maxAncestorDiff(self, root: TreeNode) -> int:
+        if not root:
+            return 0
+
+        ans = 0
+        stack = [(root, 10**6, -1)]
+
+        while stack:
+            node, minv, maxv = stack.pop()
+            minv = min(minv, node.val)
+            maxv = max(maxv, node.val)
+            if not node.left and not node.right:
+                ans = max(ans, abs(maxv - minv))
+            if node.left:
+                stack.append((node.left, minv, maxv))
+            if node.right:
+                stack.append((node.right, minv, maxv))
+
+        return ans

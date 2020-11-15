3be3c97838dc5fc5c93968cc3eff1ebb26fe8146
joshiaj7 <joshia.tan7@gmail.com>
2020-11-15 19:21:12 +0700

    improve performance

--- python3/range_sum_of_bst.py
+++ python3/range_sum_of_bst.py
@@ -16,16 +16,16 @@ class Solution:
         if not root:
             return 0
 
-        ans = []
+        ans = 0
         stack = [root]
 
         while stack:
             node = stack.pop()
             if low <= node.val <= high:
-                ans.append(node.val)
+                ans += node.val
             if node.left:
                 stack.append(node.left)
             if node.right:
                 stack.append(node.right)
 
-        return sum(ans)
+        return ans

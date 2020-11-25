af86270e30fbdf92180ec33c199d541d01ef8109
joshiaj7 <joshia.tan7@gmail.com>
2020-11-25 22:15:50 +0700

    leetcode 2020-11-25

---
+++ python3/smallest_integer_divisible_by_K.py
@@ -0,0 +1,18 @@
+"""
+Space   : O(1)
+Time    : O(n)
+"""
+
+
+class Solution:
+    def smallestRepunitDivByK(self, K: int) -> int:
+        if K % 2 == 0 or K % 5 == 0:
+            return -1
+
+        rem = 0
+        for i in range(K):
+            rem = ((rem * 10) + 1) % K
+            if rem == 0:
+                return i + 1
+
+        return -1

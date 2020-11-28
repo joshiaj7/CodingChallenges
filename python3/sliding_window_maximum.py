9e5e8f152110af3ec906d108b58f3916adb494f0
joshiaj7 <joshia.tan7@gmail.com>
2020-11-28 20:11:58 +0700

    leetcode 2020-11-28

---
+++ python3/sliding_window_maximum.py
@@ -0,0 +1,59 @@
+import collections
+
+
+class Solution:
+    def maxSlidingWindow(self, nums: List[int], k: int) -> List[int]:
+        """
+        Personal Attempt
+        Space   : O(n)
+        Time    : O(n**2)
+        """
+        if not nums:
+            return []
+
+        ans = []
+        n = len(nums)
+        dq = []
+
+        for i in range(n):
+            # check if expired
+            if dq and dq[0][1] <= i-k:
+                dq.pop(0)
+
+            # utmost left in dq is the max val
+            while dq and dq[-1][0] < nums[i]:
+                dq.pop()
+            dq.append([nums[i], i])
+
+            # start fill ans i is fulfilled
+            if i >= k - 1:
+                ans.append(dq[0][0])
+
+            # print(dq)
+
+        return ans
+
+    def maxSlidingWindowBest(self, nums: List[int], k: int) -> List[int]:
+        """
+        Best solution
+        Space   : O(n)
+        Time    : O(n**2)
+        """
+
+        window = collections.deque()
+
+        res = []
+
+        for i in range(len(nums)):
+            print(window, i-k)
+            if window and i-k >= window[0][1]:
+                window.popleft()
+
+            while window and nums[i] > window[-1][0]:
+                window.pop()
+            window.append((nums[i], i))
+
+            if i >= k - 1:
+                res.append(window[0][0])
+
+        return res

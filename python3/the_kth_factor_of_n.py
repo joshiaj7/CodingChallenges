91550a5ca5ceca36c0ec5123a31d5e5e4d6c394a
joshiaj7 <joshia.tan7@gmail.com>
2020-12-04 15:13:17 +0700

    leetcode 2020-12-4

---
+++ python3/the_kth_factor_of_n.py
@@ -0,0 +1,16 @@
+"""
+Space   : O(1)
+Time    : O(n)
+"""
+
+class Solution:
+    def kthFactor(self, n: int, k: int) -> int:
+        idx = 0
+        
+        for i in range(1, n+1):
+            if n%i == 0:
+                idx += 1
+            if idx == k:
+                return i
+        
+        return -1

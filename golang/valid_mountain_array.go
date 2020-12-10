0ce5e531b3c97f01daf61b22c08d35c4e62ae3e9
joshiaj7 <joshia.tan7@gmail.com>
2020-12-10 15:23:04 +0700

    leetcode 2020-12-10

---
+++ golang/valid_mountain_array.go
@@ -0,0 +1,35 @@
+package golang
+
+func validMountainArray(arr []int) bool {
+	n := len(arr)
+
+	if n < 3 {
+		return false
+	}
+
+	peak := 0
+	ind := "+"
+
+	for i := 1; i < n; i++ {
+		if (arr[i]-arr[i-1] > 0) && ind == "-" {
+			return false
+		} else if arr[i]-arr[i-1] == 0 {
+			return false
+		} else if arr[i]-arr[i-1] < 0 {
+			ind = "-"
+		}
+
+		if i > 1 {
+			if (arr[i]-arr[i-1] < 0) && (arr[i-1]-arr[i-2] > 0) {
+				peak++
+			}
+		}
+
+	}
+
+	if peak == 1 {
+		return true
+	}
+
+	return false
+}

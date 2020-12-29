066522df857d4fb8bf65d0a4e7ebc064160d9f1a
joshiaj7 <joshia.tan7@gmail.com>
2020-12-29 16:08:44 +0700

    leetcode 2020-12-29

---
+++ python3/pseudo-palindromic_paths_in_a_binary_tree.py
@@ -0,0 +1,48 @@
+"""
+BFS Solution
+Space   : O(n)
+Time    : O(n)
+"""
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+
+class Solution:
+    def pseudoPalindromicPaths(self, root: TreeNode) -> int:
+        if not root:
+            return 0
+
+        stack = [(root, [])]
+        res = []
+        ans = 0
+
+        while stack:
+            node, mem = stack.pop()
+            if not node.left and not node.right:
+                res.append(mem + [node.val])
+                continue
+            if node.left:
+                stack.append((node.left, mem + [node.val]))
+            if node.right:
+                stack.append((node.right, mem + [node.val]))
+
+        for item in res:
+            d = {}
+            for i in item:
+                if i in d:
+                    d[i] += 1
+                else:
+                    d[i] = 1
+            odds = 0
+            for _, v in d.items():
+                if v % 2 == 1:
+                    odds += 1
+            if odds <= 1:
+                ans += 1
+
+        return ans

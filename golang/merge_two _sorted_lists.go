5e4a899e05d1caff6108e07828422faf201523cc
joshiaj7 <joshia.tan7@gmail.com>
2021-01-04 15:26:04 +0700

    add go ver

---
+++ golang/merge_two _sorted_lists.go
@@ -0,0 +1,46 @@
+package golang
+
+import (
+	"golang/model"
+)
+
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+
+func mergeTwoLists(l1 *model.ListNode, l2 *model.ListNode) *model.ListNode {
+	p1, p2 := l1, l2
+	ans := &model.ListNode{
+		Val: 0,
+	}
+	pans := ans
+
+	for p1 != nil && p2 != nil {
+		if p1.Val < p2.Val {
+			pans.Next = &model.ListNode{
+				Val: p1.Val,
+			}
+			p1 = p1.Next
+		} else {
+			pans.Next = &model.ListNode{
+				Val: p2.Val,
+			}
+			p2 = p2.Next
+		}
+		pans = pans.Next
+	}
+
+	if p1 != nil {
+		pans.Next = p1
+	}
+
+	if p2 != nil {
+		pans.Next = p2
+	}
+
+	return ans.Next
+}

b3464ee17b78f5402a73fe7c0b639309af52f7e3
joshiaj7 <joshia.tan7@gmail.com>
2021-01-05 12:20:59 +0700

    code drill

---
+++ python3/brick_wall.py
@@ -0,0 +1,22 @@
+class Solution:
+    def leastBricks(self, wall: List[List[int]]) -> int:
+        if not wall:
+            return 0
+
+        d = {}
+
+        for x in wall:
+            width = 0
+            for i, b in enumerate(x):
+                width += b
+                if i < len(x)-1:
+                    if width not in d:
+                        d[width] = 1
+                    else:
+                        d[width] += 1
+
+        ans = len(wall)
+        for _, v in d.items():
+            ans = min(len(wall) - v, ans)
+
+        return ans

cb2e990f5144e8bc037bbe6da168f8425591b90e
joshiaj7 <joshia.tan7@gmail.com>
2021-01-05 14:14:16 +0700

    code drill

---
+++ golang/remove_nth_node_from_end_of_list.go
@@ -0,0 +1,43 @@
+package golang
+
+import (
+	"golang/model"
+)
+
+"""
+Space	: O(n)
+Time	: O(n)
+"""
+
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+func removeNthFromEnd(head *model.ListNode, n int) *model.ListNode {
+	var mem []int
+	phead := head
+
+	for phead != nil {
+		mem = append(mem, phead.Val)
+		phead = phead.Next
+	}
+
+	ans := &model.ListNode{
+		Val: 0,
+	}
+	pans := ans
+
+	for i := 0; i < len(mem); i++ {
+		if i != len(mem)-n {
+			pans.Next = &model.ListNode{
+				Val: mem[i],
+			}
+			pans = pans.Next
+		}
+	}
+
+	return ans.Next
+}

be3eb89f942338b1b0bf87de7bc817b121b49c33
joshiaj7 <joshia.tan7@gmail.com>
2021-01-11 15:50:20 +0700

    leetcode 2021-1-11

---
+++ golang/merge_sorted_array.go
@@ -0,0 +1,32 @@
+package golang
+
+// Space	: O(m + n)	
+// Time		: O(m + n)
+
+
+func merge(nums1 []int, m int, nums2 []int, n int)  {
+    var temp []int
+    i, j := 0, 0
+    
+    for i < m && j < n {
+        if nums1[i] < nums2[j] {
+            temp = append(temp, nums1[i])
+            i++
+        } else {
+            temp = append(temp, nums2[j])
+            j++
+        }
+    }
+    
+    if i < m {
+        temp = append(temp, nums1[i:m]...)
+    } 
+    
+    if j < n {
+        temp = append(temp, nums2[j:n]...)
+    }
+    
+    for k := 0; k < m+n; k++ {
+        nums1[k] = temp[k]
+    }
+}

d244495b0aee8edeb6e829ccffcd4e0d48869e29
joshiaj7 <joshia.tan7@gmail.com>
2021-01-29 16:33:14 +0700

    leetcode 2021-1-29

--- golang/functions.go
+++ golang/functions.go
@@ -1,9 +1,17 @@
 package golang
 
 // Max compare 2 int and return the highest value
-func max(a int, b int) int {
+func Max(a int, b int) int {
 	if a > b {
 		return a
 	}
 	return b
+}
+
+// Min compare 2 int and return the lowest value
+func Min(a int, b int) int {
+    if a > b {
+        return b
+    }
+    return a
 }
---
+++ python3/vertical_order_traversal_of_a_binary_tree.py
@@ -0,0 +1,36 @@
+from collections import defaultdict
+
+
+class Solution:
+    def verticalTraversal(self, root: TreeNode) -> List[List[int]]:
+        d = defaultdict(list)
+        stack = [(root, 0)]
+        minv, maxv = 0, 0
+
+        while stack:
+            stemp = []
+            dtemp = {}
+            for node, pos in stack:
+                minv = min(minv, pos)
+                maxv = max(maxv, pos)
+
+                if pos not in dtemp:
+                    dtemp[pos] = [node.val]
+                else:
+                    dtemp[pos].append(node.val)
+
+                if node.left:
+                    stemp.append((node.left, pos - 1))
+                if node.right:
+                    stemp.append((node.right, pos + 1))
+
+            for k, v in dtemp.items():
+                d[k].extend(sorted(v))
+
+            stack = stemp
+
+        ans = []
+        for i in range(minv, maxv+1):
+            ans.append(d[i])
+
+        return ans

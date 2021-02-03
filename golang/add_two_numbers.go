d84fa6d0a4b561d8ffbb9da60eb85b92645ef833
joshiaj7 <joshia.tan7@gmail.com>
2021-02-03 15:17:13 +0700

    clear warning and reformat

--- golang/add_two_numbers.go
+++ golang/add_two_numbers.go
@@ -5,46 +5,46 @@ package golang
 
 import "golang/model"
 
- func addTwoNumbers(l1 *model.ListNode, l2 *model.ListNode) *model.ListNode {
-    ans := &model.ListNode{
-        Val: 0,
-    }
-    p := ans
-    carry := 0
+func addTwoNumbers(l1 *model.ListNode, l2 *model.ListNode) *model.ListNode {
+	ans := &model.ListNode{
+		Val: 0,
+	}
+	p := ans
+	carry := 0
 
-    for l1 != nil || l2 != nil {
-        v1, v2 := 0, 0
-        
-        if l1 != nil {
-            v1 = l1.Val
-        }
-        
-        if l2 != nil {
-            v2 = l2.Val
-        }
-        
-        v := (v1 + v2 + carry) % 10
-        carry = (v1 + v2 + carry) / 10
-        
-        p.Next = &model.ListNode{
-            Val: v,
-        }
-        p = p.Next
-        
-        if l1 != nil {
-            l1 = l1.Next
-        }
-        
-        if l2 != nil {
-            l2 = l2.Next
-        }
-    }
-    
-    if carry != 0 {
-        p.Next = &model.ListNode{
-            Val: carry,
-        }
-    }
-    
-    return ans.Next
+	for l1 != nil || l2 != nil {
+		v1, v2 := 0, 0
+
+		if l1 != nil {
+			v1 = l1.Val
+		}
+
+		if l2 != nil {
+			v2 = l2.Val
+		}
+
+		v := (v1 + v2 + carry) % 10
+		carry = (v1 + v2 + carry) / 10
+
+		p.Next = &model.ListNode{
+			Val: v,
+		}
+		p = p.Next
+
+		if l1 != nil {
+			l1 = l1.Next
+		}
+
+		if l2 != nil {
+			l2 = l2.Next
+		}
+	}
+
+	if carry != 0 {
+		p.Next = &model.ListNode{
+			Val: carry,
+		}
+	}
+
+	return ans.Next
 }
--- golang/smallest_string_with_a_given_numeric_value.go
+++ golang/smallest_string_with_a_given_numeric_value.go
@@ -1,25 +1,26 @@
 package golang
 
 import (
-    "strings"
+	"strconv"
+	"strings"
 )
 
 func getSmallestString(n int, k int) string {
-    zs := 0
-    
-    for k - 26 >= n - 1 {
-        zs++
-        k-=26
-        n--
-    }
-    
-    var mid string
-    if k > n {
-        mid = string(96 + k - n + 1)
-        k = k - n + 1
-        n--
-    }
-    
-    return strings.Repeat("a", n) + mid + strings.Repeat("z", zs)
-    
+	zs := 0
+
+	for k-26 >= n-1 {
+		zs++
+		k -= 26
+		n--
+	}
+
+	var mid string
+	if k > n {
+		mid = strconv.Itoa(96 + k - n + 1)
+		k = k - n + 1
+		n--
+	}
+
+	return strings.Repeat("a", n) + mid + strings.Repeat("z", zs)
+
 }

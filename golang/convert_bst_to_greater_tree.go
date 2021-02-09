3a3613e790a8cf92543c371c58af499bf6150b06
joshiaj7 <joshia.tan7@gmail.com>
2021-02-09 16:17:45 +0700

    leetcode 2021-2-9

---
+++ golang/convert_bst_to_greater_tree.go
@@ -0,0 +1,37 @@
+package golang
+
+import (
+	"golang/model"
+)
+
+func getInorder(root *model.TreeNode) []int {
+	res := []int{}
+
+	if root != nil {
+		res = append(res, getInorder(root.Left)...)
+		res = append(res, root.Val)
+		res = append(res, getInorder(root.Right)...)
+	}
+
+	return res
+}
+
+func insertGreaterTree(root *model.TreeNode, nodes []int) {
+	if root != nil && len(nodes) > 0 {
+		insertGreaterTree(root.Left, nodes)
+		root.Val = nodes[0]
+		nodes = append(nodes[:0], nodes[1:]...)
+		insertGreaterTree(root.Right, nodes)
+	}
+}
+
+func convertBST(root *model.TreeNode) *model.TreeNode {
+	nodes := getInorder(root)
+	for i := len(nodes) - 2; i >= 0; i-- {
+		nodes[i] += nodes[i+1]
+	}
+
+	point := root
+	insertGreaterTree(point, nodes)
+	return root
+}
---
+++ python3/convert_bst_to_greater_tree.py
@@ -0,0 +1,29 @@
+import TreeNode
+
+class Solution:
+    def getInorder(self, root: TreeNode) -> List[int]:
+        res = []
+        
+        if root:
+            res += self.getInorder(root.left)
+            res.append(root.val)
+            res += self.getInorder(root.right)
+            
+        return res
+    
+    def convertBST(self, root: TreeNode) -> TreeNode:
+        
+        def insertGreaterTree(point, nodes):
+            if nodes and point:
+                insertGreaterTree(point.left, nodes)
+                point.val = nodes.pop(0)
+                insertGreaterTree(point.right, nodes)
+        
+        nodes = self.getInorder(root)
+        for i in range(len(nodes)-2,-1,-1):
+            nodes[i] += nodes[i+1]
+        
+        p = root
+        insertGreaterTree(p, nodes)
+        
+        return root

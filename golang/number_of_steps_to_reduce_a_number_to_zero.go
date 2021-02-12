96f7a19a012790deec2cad0a2eceb1339f57aa43
joshiaj7 <joshia.tan7@gmail.com>
2021-02-12 16:03:06 +0700

    leetcode 2021-2-12

---
+++ golang/number_of_steps_to_reduce_a_number_to_zero.go
@@ -0,0 +1,19 @@
+package golang
+
+// Space	: O(1)
+// Time		: O(n)
+
+func numberOfSteps (num int) int {
+    ans := 0
+    
+    for num != 0 {
+        if num % 2 == 0 { 
+            num /= 2
+        } else {
+            num--
+        }
+        ans++
+    }
+    
+    return ans
+}
---
+++ python3/number_of_steps_to_reduce_a_number_to_zero.py
@@ -0,0 +1,18 @@
+"""
+Space   : O(1)
+Time    : O(n)
+"""
+
+
+class Solution:
+    def numberOfSteps(self, num: int) -> int:
+        ans = 0
+
+        while num != 0:
+            if num % 2 == 0:
+                num //= 2
+            else:
+                num -= 1
+            ans += 1
+
+        return ans

8fd19a6ade7e20ade4f2c42150cffd791317cf45
joshiaj7 <joshia.tan7@gmail.com>
2021-03-05 19:47:26 +0700

    leetcode 2021-3-4

---
+++ golang/intersection_of_two_linked_lists.go
@@ -0,0 +1,55 @@
+package golang
+
+// Space	: O(1)
+// Time	: O(n)
+
+
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+func getIntersectionNode(headA, headB *ListNode) *ListNode {
+    la, lb := 0, 0
+    pa, pb := headA, headB
+    
+    for pa != nil {
+        la++
+        pa = pa.Next
+    }
+    
+    for pb != nil {
+        lb++
+        pb = pb.Next
+    }
+    
+    pa, pb = headA, headB
+    
+    dif := 0
+    if la > lb {
+        dif = la - lb
+        for dif > 0 {
+            pa = pa.Next
+            dif--
+        }
+    } else {
+        dif = lb - la
+        for dif > 0 {
+            pb = pb.Next
+            dif--
+        }
+    }
+    
+    for pa != nil {
+        if pa == pb {
+            return pa
+        }
+        pa = pa.Next
+        pb = pb.Next
+    }
+    
+    return nil
+    
+}

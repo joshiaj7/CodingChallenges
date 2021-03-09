46e5398c8237c4d93132165b01a36f2261be36a8
joshiaj7 <joshia.tan7@gmail.com>
2021-03-09 21:51:07 +0700

    leetcode 2021-3-9

---
+++ golang/add_one_row_to_tree.go
@@ -0,0 +1,67 @@
+package golang
+
+// Space	: O(n)
+// Time		: O(n)
+
+import (
+	"golang/model"
+)
+
+ func addOneRow(root *model.TreeNode, v int, d int) *model.TreeNode {
+    if d == 1 {
+        ans := &model.TreeNode{
+            Val : v,
+            Left : root,
+        }
+        return ans
+    }
+    
+    level := 1 
+    stack := []*model.TreeNode{root}
+    
+    for len(stack) > 0 {
+        temp := []*model.TreeNode{}
+        if level + 1 == d {
+            for _, node := range stack {
+                if node.Left != nil {
+                    l := node.Left
+                    node.Left = &model.TreeNode{
+                        Val: v,
+                        Left: l,
+                    }    
+                } else {
+                    node.Left = &model.TreeNode{
+                        Val: v,
+                    } 
+                }
+                
+                if node.Right != nil {
+                    r := node.Right
+                    node.Right = &model.TreeNode{
+                        Val: v,
+                        Right: r,
+                    }    
+                } else {
+                    node.Right = &model.TreeNode{
+                        Val: v,
+                    } 
+                }
+            }
+            break
+        }
+        for _, node := range stack{
+            if node.Left != nil {
+                temp = append(temp, node.Left)
+            }
+            if node.Right != nil {
+                temp = append(temp, node.Right)
+            }
+        }
+
+        stack = temp
+        level++
+    }
+       
+    return root
+}
+
---
+++ python3/add_one_row_to_tree.py
@@ -0,0 +1,46 @@
+from model import TreeNode
+
+"""
+Space   : O(n)
+Time    : O(n)
+"""
+
+
+class Solution:
+    def addOneRow(self, root: TreeNode, v: int, d: int) -> TreeNode:
+        if d == 1:
+            ans = TreeNode(v)
+            ans.left = root
+            return ans
+
+        level = 1
+        stack = [root]
+
+        while stack:
+            temp = []
+            if level + 1 == d:
+                for node in stack:
+                    if node.left:
+                        l = node.left
+                        node.left = TreeNode(v)
+                        node.left.left = l
+                    else:
+                        node.left = TreeNode(v)
+                    if node.right:
+                        r = node.right
+                        node.right = TreeNode(v)
+                        node.right.right = r
+                    else:
+                        node.right = TreeNode(v)
+                break
+
+            for node in stack:
+                if node.left:
+                    temp.append(node.left)
+                if node.right:
+                    temp.append(node.right)
+
+            stack = temp
+            level += 1
+
+        return root

1b96947bdfad43b1827277686c3a045e7bcbab04
joshiaj7 <joshia.tan7@gmail.com>
2021-03-14 15:31:16 +0700

    leetcode 2021-3-14

---
+++ golang/swapping_nodes_in_a_linked_list.go
@@ -0,0 +1,31 @@
+package golang
+
+import (
+	"golang/model"
+)
+
+// Space	: O(n)
+// Time		: O(n)
+
+
+func swapNodes(head *model.ListNode, k int) *model.ListNode {
+    temp := []int{}
+    p := head
+    
+    for p != nil {
+        temp = append(temp, p.Val)
+        p = p.Next
+    }
+    
+    temp[k-1], temp[len(temp)-k] = temp[len(temp)-k], temp[k-1]
+    
+    p = head
+    i := 0
+    for i < len(temp){
+        p.Val = temp[i]
+        i++
+        p = p.Next
+    }
+    
+    return head
+}
---
+++ python3/swapping_nodes_in_a_linked_list.py
@@ -0,0 +1,27 @@
+from model import ListNode
+
+"""
+Space   : O(n)
+Time    : O(n)
+"""
+
+
+class Solution:
+    def swapNodes(self, head: ListNode, k: int) -> ListNode:
+        p = head
+        temp = []
+
+        while p:
+            temp.append(p.val)
+            p = p.next
+
+        temp[k-1], temp[~(k-1)] = temp[~(k-1)], temp[k-1]
+
+        p = head
+        i = 0
+        while p:
+            p.val = temp[i]
+            i += 1
+            p = p.next
+
+        return head

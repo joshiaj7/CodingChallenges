197ae735394796f35bfde33b3441c36d523837a1
joshiaj7 <joshia.tan7@gmail.com>
2021-04-08 15:52:07 +0700

    fix golang module

--- golang/determine_if_string_halves_are_alike.go
+++ golang/determine_if_string_halves_are_alike.go
@@ -1,34 +1,32 @@
 package golang
 
 // Space 	: O(1)
-// Time	: O(n)
+// Time	    : O(n)
 
 import (
-    "fmt"
-    "unicode"
+	"unicode"
 )
 
 func halvesAreAlike(s string) bool {
-    v1, v2 := 0, 0
-    n := len(s)
-    vowels := map[rune]int{
-        'a': 1,
-        'e': 1,
-        'i': 1,
-        'o': 1,
-        'u': 1,
-    }
-    
-    
-    for i := 0; i < n/2; i++ {
-        if _, ok := vowels[unicode.ToLower(rune(s[i]))]; ok {
-            v1++
-        } 
-        if _, ok := vowels[unicode.ToLower(rune(s[i+(n/2)]))]; ok {
-            v2++
-        } 
-        
-    }
+	v1, v2 := 0, 0
+	n := len(s)
+	vowels := map[rune]int{
+		'a': 1,
+		'e': 1,
+		'i': 1,
+		'o': 1,
+		'u': 1,
+	}
 
-    return v1 == v2
+	for i := 0; i < n/2; i++ {
+		if _, ok := vowels[unicode.ToLower(rune(s[i]))]; ok {
+			v1++
+		}
+		if _, ok := vowels[unicode.ToLower(rune(s[i+(n/2)]))]; ok {
+			v2++
+		}
+
+	}
+
+	return v1 == v2
 }
--- golang/integer_to_roman.go
+++ golang/integer_to_roman.go
@@ -1,51 +1,53 @@
+package golang
+
 import (
-    "math"
-    "strconv"
+	"fmt"
+	"math"
+	"strconv"
 )
 
 func intToRoman(num int) string {
-    ans := ""
-    truth := map[int]string{
-        1: "I",
-        4: "IV",
-        5: "V",
-        9: "IX",
-        10: "X",
-        40: "XL",
-        50: "L",
-        90: "XC",
-        100: "C",
-        400: "CD",
-        500: "D",
-        900: "CM",
-        1000: "M",
-    }
-    
-    n := len(strconv.Itoa(num))
-    multi := int(math.Pow(10, float64(n-1)))
-    
-    for i := 0; i < n; i++ {
-        temp, _ := strconv.Atoi(string(strconv.Itoa(num)[i]))
-        fmt.Println(temp)
-        
-        for temp > 0 {
-            if temp == 9 {
-                ans += truth[9 * multi]
-                temp -= 9 
-            } else if temp >= 5 {
-                ans += truth[5 * multi]
-                temp -= 5
-            } else if temp >= 4 {
-                ans += truth[4 * multi]
-                temp -= 4        
-            } else if temp >= 1 {
-                ans += truth[1 * multi]
-                temp -= 1             
-            }
-        }
-        multi /= 10
-    }
-        
+	ans := ""
+	truth := map[int]string{
+		1:    "I",
+		4:    "IV",
+		5:    "V",
+		9:    "IX",
+		10:   "X",
+		40:   "XL",
+		50:   "L",
+		90:   "XC",
+		100:  "C",
+		400:  "CD",
+		500:  "D",
+		900:  "CM",
+		1000: "M",
+	}
 
-    return ans
+	n := len(strconv.Itoa(num))
+	multi := int(math.Pow(10, float64(n-1)))
+
+	for i := 0; i < n; i++ {
+		temp, _ := strconv.Atoi(string(strconv.Itoa(num)[i]))
+		fmt.Println(temp)
+
+		for temp > 0 {
+			if temp == 9 {
+				ans += truth[9*multi]
+				temp -= 9
+			} else if temp >= 5 {
+				ans += truth[5*multi]
+				temp -= 5
+			} else if temp >= 4 {
+				ans += truth[4*multi]
+				temp -= 4
+			} else if temp >= 1 {
+				ans += truth[1*multi]
+				temp -= 1
+			}
+		}
+		multi /= 10
+	}
+
+	return ans
 }

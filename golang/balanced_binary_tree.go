da6c6500cc7a483abd7a21077fda2c01e07b879e
joshiaj7 <joshia.tan7@gmail.com>
2021-04-08 16:02:01 +0700

    test local user

--- golang/balanced_binary_tree.go
+++ golang/balanced_binary_tree.go
@@ -1,50 +1,46 @@
 package golang
 
-/*
-	Space	: O(1)
-	Time	: O(n)
- */
+// Space: O(1)
+// Time	: O(n)
 
 import (
 	"golang/model"
 )
 
-
 func checkBalanced(root *model.TreeNode, res bool) (int, bool) {
-    if root == nil {
-        return 0, res
-    }
-    
-    l, lr := checkBalanced(root.Left, res)
-    if !lr {
-        return 0, false
-    }
-    
-    r, rr := checkBalanced(root.Right, res)
-    if !rr {
-        return 0, false
-    }
-    
-    if l > r {
-        if l - r > 1 {
-            res = false
-        }  
-        return l + 1, res
-	} 
+	if root == nil {
+		return 0, res
+	}
 
-	if r - l > 1 {
+	l, lr := checkBalanced(root.Left, res)
+	if !lr {
+		return 0, false
+	}
+
+	r, rr := checkBalanced(root.Right, res)
+	if !rr {
+		return 0, false
+	}
+
+	if l > r {
+		if l-r > 1 {
+			res = false
+		}
+		return l + 1, res
+	}
+
+	if r-l > 1 {
 		res = false
-	}  
+	}
 	return r + 1, res
-    
+
 }
 
-
 func isBalanced(root *model.TreeNode) bool {
-    if root == nil {
-        return true
-    }
+	if root == nil {
+		return true
+	}
 
-    _, res := checkBalanced(root, true)
-    return res
+	_, res := checkBalanced(root, true)
+	return res
 }

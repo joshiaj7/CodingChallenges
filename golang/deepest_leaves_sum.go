cab88b0dd1562df873b837611e8d03093a0d9114
joshiaj7 <joshia.tan7@gmail.com>
2021-04-11 19:37:29 +0700

    leetcode 2021-4-11

---
+++ golang/deepest_leaves_sum.go
@@ -0,0 +1,32 @@
+package golang
+
+import (
+	"golang/model"
+)
+
+
+func deepestLeavesSum(root *model.TreeNode) int {
+    stack := []*model.TreeNode{root}
+    
+    for len(stack) > 0 {
+        temp := []*model.TreeNode{}
+        ans := 0
+        for _, node := range stack {
+            if node.Left != nil {
+                temp = append(temp, node.Left)
+            }
+            if node.Right != nil {
+                temp = append(temp, node.Right)
+            }
+            ans += node.Val
+        }
+        
+        if len(temp) == 0 {
+            return ans
+        }
+        
+        stack = temp
+    }
+    
+    return 0
+}

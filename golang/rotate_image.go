c1c3fd613228f41c92ededf8825cb4743f4cf4d5
joshiaj7 <joshia.tan7@gmail.com>
2021-04-26 15:50:50 +0700

    leetcode 2021-4-25

---
+++ golang/rotate_image.go
@@ -0,0 +1,27 @@
+package golang
+
+// Space : O(1)
+// Time : O(n^2)
+
+func rotateMatrix(matrix [][]int) {
+	n := len(matrix)
+
+	for i := 0; i < n; i++ {
+		for j := 0; j < n; j++ {
+			if i < j {
+				matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
+			}
+		}
+	}
+
+	row := 0
+	for row < n {
+		s, e := 0, n-1
+		for s < e {
+			matrix[row][s], matrix[row][e] = matrix[row][e], matrix[row][s]
+			s++
+			e--
+		}
+		row++
+	}
+}
---
+++ python3/rotate_image.py
@@ -0,0 +1,18 @@
+# Space : O(1)
+# Time : O(n^2)
+
+class Solution:
+    def rotate(self, matrix: List[List[int]]) -> None:
+        """
+        Do not return anything, modify matrix in-place instead.
+        """
+        n = len(matrix)
+        
+        for i in range(n):
+            for j in range(n):
+                if i < j:
+                    matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
+                
+        for x in matrix:
+            x.reverse()
+        

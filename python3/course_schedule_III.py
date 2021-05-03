ac64ba994b9b063be44189d1801bfe415fdd3356
joshiaj7 <joshia.tan7@gmail.com>
2021-05-03 14:36:52 +0700

    leetcode 2021-5-2

---
+++ python3/course_schedule_III.py
@@ -0,0 +1,18 @@
+import heapq
+
+# Space : O(n)
+# Time  : O(n log n)
+
+class Solution:
+    def scheduleCourse(self, courses: List[List[int]]) -> int:
+        courses.sort(key=lambda x: x[1])
+        heap = []
+        time = 0
+        
+        for dur, last in courses:
+            heapq.heappush(heap, -dur)
+            time += dur
+            if time > last:
+                time += heapq.heappop(heap)
+        
+        return len(heap)

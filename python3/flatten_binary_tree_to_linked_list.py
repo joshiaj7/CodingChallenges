b5f713cb9952aa82f973edb05eadfc208599fe1c
joshiaj7 <joshia.tan7@gmail.com>
2021-05-17 11:49:08 +0700

    remove comments and add docs

--- python3/flatten_binary_tree_to_linked_list.py
+++ python3/flatten_binary_tree_to_linked_list.py
@@ -1,11 +1,8 @@
 from model import TreeNode
 
-# Definition for a binary tree node.
-# class TreeNode:
-#     def __init__(self, val=0, left=None, right=None):
-#         self.val = val
-#         self.left = left
-#         self.right = right
+# Space : O(n)
+# Time  : O(n)
+
 class Solution:
     def getInorder(self, root: TreeNode) -> List[int]:
         res = []

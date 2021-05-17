3e7fbfcd3f3ebd854033ae631908b24cf89c5b2a
joshiaj7 <joshia.tan7@gmail.com>
2021-05-17 11:41:46 +0700

    leetcode 2021-5-14

---
+++ golang/flatten_binary_tree_to_linked_list.go
@@ -0,0 +1,37 @@
+package golang
+
+import {
+	"golang/model"
+}
+
+func getTreePreorder(root *model.TreeNode) []int {
+	res := []int{}
+
+	if root != nil {
+		res = append(res, root.Val)
+		if root.Left != nil {
+			res = append(res, getTreePreorder(root.Left)...)
+		}
+		if root.Right != nil {
+			res = append(res, getTreePreorder(root.Right)...)
+		}
+	}
+
+	return res
+}
+
+func flatten(root *model.TreeNode) {
+	preorder := getTreePreorder(root)
+
+	p := root
+	for i, val := range preorder {
+		p.Val = val
+		p.Left = nil
+		if i != len(preorder)-1 {
+			p.Right = &model.TreeNode{}
+		}
+
+		p = p.Right
+	}
+
+}

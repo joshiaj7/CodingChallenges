44826ffdd80c50f645af24ce9bac61e0d71cd100
joshiaj7 <joshia.tan7@gmail.com>
2021-06-08 15:17:33 +0700

    leetcode drill

---
+++ golang/implement_queue_using_stacks.go
@@ -0,0 +1,43 @@
+package main
+
+type MyQueue struct {
+	Q []int
+}
+
+/** Initialize your data structure here. */
+func Constructor() MyQueue {
+	return MyQueue{
+		Q: []int{},
+	}
+}
+
+/** Push element x to the back of queue. */
+func (this *MyQueue) Push(x int) {
+	this.Q = append(this.Q, x)
+}
+
+/** Removes the element from in front of queue and returns that element. */
+func (this *MyQueue) Pop() int {
+	num := this.Q[0]
+	this.Q = this.Q[1:]
+	return num
+}
+
+/** Get the front element. */
+func (this *MyQueue) Peek() int {
+	return this.Q[0]
+}
+
+/** Returns whether the queue is empty. */
+func (this *MyQueue) Empty() bool {
+	return len(this.Q) == 0
+}
+
+/**
+ * Your MyQueue object will be instantiated and called as such:
+ * obj := Constructor();
+ * obj.Push(x);
+ * param_2 := obj.Pop();
+ * param_3 := obj.Peek();
+ * param_4 := obj.Empty();
+ */

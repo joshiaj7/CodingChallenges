d7eaa92a4f86ca0879eacc983ec7d1e243b874c4
joshiaj7 <joshia.tan7@gmail.com>
2021-06-10 15:28:38 +0700

    leetcode 2021-6-10

---
+++ python3/my_calendar_i.py
@@ -0,0 +1,25 @@
+from bisect import bisect_right
+
+# Space : O(n)
+# Time  : O(log n)
+
+
+class MyCalendar:
+
+    def __init__(self):
+        self.calendar = []
+
+    def book(self, start: int, end: int) -> bool:
+        lhs = bisect_right(self.calendar, start)
+
+        if self.calendar and lhs < len(self.calendar) and ((lhs % 2) == 1 or self.calendar[lhs] < end):
+            return False
+
+        self.calendar.insert(lhs, end)
+        self.calendar.insert(lhs, start)
+        return True
+
+
+# Your MyCalendar object will be instantiated and called as such:
+# obj = MyCalendar()
+# param_1 = obj.book(start,end)

67bf3fc2aed132ee70a93373d80c8cfbb6e68222
joshiaj7 <joshia.tan7@gmail.com>
2021-07-10 14:33:13 +0700

    leetcode 2021-7-10

---
+++ python3/longest_increasing_subsequence.py
@@ -0,0 +1,16 @@
+from bisect import bisect_left
+
+# Space : O(n)
+# Time  : O(n log n)
+
+
+class Solution:
+    def lengthOfLIS(self, nums: List[int]) -> int:
+        sub = []
+        for num in nums:
+            i = bisect_left(sub, num)
+            if i == len(sub):
+                sub.append(num)
+            else:
+                sub[i] = num
+        return len(sub)

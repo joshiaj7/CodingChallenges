c1f199a2d2839fde0f500e206e94ae111b2b78d1
joshiaj7 <joshia.tan7@gmail.com>
2021-10-04 15:56:17 +0700

    bfs drill

---
+++ python3/flood_fill.py
@@ -0,0 +1,24 @@
+# Space : O(1)
+# Time  : O(n * m)
+
+class Solution:
+    def floodFill(self, image: List[List[int]], sr: int, sc: int, newColor: int) -> List[List[int]]:
+        coord = [(1, 0), (0, 1), (-1, 0), (0, -1)]
+        rows = len(image)
+        cols = len(image[0])
+        start = image[sr][sc]
+        
+        def crawl(x, y):
+            if image[y][x] == start:
+                image[y][x] = newColor
+            for fy, fx in coord:
+                if x+fx == -1 or x+fx == cols or y+fy == -1 or y+fy == rows:
+                    continue
+                if image[y+fy][x+fx] != start or image[y+fy][x+fx] == newColor:
+                    continue
+                crawl(x+fx, y+fy)
+                    
+        crawl(sc, sr)
+        return image    
+        
+        

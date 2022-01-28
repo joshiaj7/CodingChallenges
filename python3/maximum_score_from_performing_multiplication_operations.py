c57b15fe2ecd17abb27e56f9d132346d03bd2246
joshiaj7 <joshia.tan7@gmail.com>
2022-01-28 15:13:02 +0700

    dp drill

---
+++ python3/maximum_score_from_performing_multiplication_operations.py
@@ -0,0 +1,19 @@
+# Time  : O(m^2)
+# Space : O(m^2)
+
+class Solution:
+    def maximumScore(self, nums: List[int], multipliers: List[int]) -> int:
+        n, m = len(nums), len(multipliers)
+        dp = [[0] * (m + 1) for _ in range(m + 1)]
+     
+        for i in range(m - 1, -1, -1):
+            for left in range(i, -1, -1):
+                print(i, left)
+                mult = multipliers[i]
+                right = n - 1 - (i - left)
+                dp[i][left] = max(mult * nums[left] + dp[i + 1][left + 1], 
+                                  mult * nums[right] + dp[i + 1][left])        
+        
+        for x in dp:
+            print(x)
+        return dp[0][0]

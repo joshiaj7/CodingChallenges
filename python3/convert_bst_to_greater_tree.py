1b4f53ea267dc2e743b7f96f06a893665cd0c79a
joshiaj7 <joshia.tan7@gmail.com>
2022-04-16 20:11:58 +0700

    leetcode 2022-04-16

--- python3/convert_bst_to_greater_tree.py
+++ python3/convert_bst_to_greater_tree.py
@@ -1,32 +1,21 @@
 from .model import TreeNode
 
-# Space : O(n)
+# Space : O(1)
 # Time  : O(n)
 
+
 class Solution:
-    def getInorder(self, root: TreeNode) -> List[int]:
-        res = []
-        
-        if root:
-            res += self.getInorder(root.left)
-            res.append(root.val)
-            res += self.getInorder(root.right)
-            
-        return res
-    
-    def convertBST(self, root: TreeNode) -> TreeNode:
-        
-        def insertGreaterTree(point, nodes):
-            if nodes and point:
-                insertGreaterTree(point.left, nodes)
-                point.val = nodes.pop(0)
-                insertGreaterTree(point.right, nodes)
-        
-        nodes = self.getInorder(root)
-        for i in range(len(nodes)-2,-1,-1):
-            nodes[i] += nodes[i+1]
-        
-        p = root
-        insertGreaterTree(p, nodes)
-        
+    def convertBST(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
+
+        def convert(root, total):
+            if not root:
+                return total
+
+            total = convert(root.right, total)
+            root.val += total
+            total = convert(root.left, root.val)
+
+            return total
+
+        convert(root, 0)
         return root

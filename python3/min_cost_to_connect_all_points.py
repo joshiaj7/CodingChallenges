920edf81af3aad385aa4941f15c9bf63c412dfa2
joshiaj7 <joshia.tan7@gmail.com>
2022-04-26 21:02:22 +0700

    leetcode 2022-04-26

---
+++ python3/min_cost_to_connect_all_points.py
@@ -0,0 +1,20 @@
+# Space : O(n)
+# Time  : O(n^2)
+
+# Minimum Spanning Tree
+# hackerearth.com/practice/algorithms/graphs/minimum-spanning-tree/tutorial/
+
+class Solution:
+    def minCostConnectPoints(self, points: List[List[int]]) -> int:
+        d = {(x, y): float('inf') if i else 0 for i,
+             (x, y) in enumerate(points)}
+
+        ans = 0
+        while d:
+            x1, y1 = min(d, key=d.get)
+            ans += d.pop((x1, y1))
+
+            for x2, y2 in d:
+                d[(x2, y2)] = min(d[(x2, y2)], (abs(x1 - x2) + abs(y1 - y2)))
+
+        return ans

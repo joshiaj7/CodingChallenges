b4d85c68dca85f499ef8877dcc8ccf8c635932b8
joshiaj7 <joshia.tan7@gmail.com>
2022-05-20 20:47:09 +0700

    leetcode 2022-05-20

--- python3/unique_paths_II.py
+++ python3/unique_paths_II.py
@@ -1,22 +1,34 @@
-# Space : O(1) 
-# Time  : O(n*m)
+"""
+Space : O(1) 
+Time  : O(n*m)
+
+Method:
+Dynamic Programming (DP)
+"""
+
 
 class Solution:
     def uniquePathsWithObstacles(self, obstacleGrid: List[List[int]]) -> int:
         row = len(obstacleGrid)
         col = len(obstacleGrid[0])
-        
-        if obstacleGrid[0][0] == 1:
+
+        if obstacleGrid[0][0] == 1 or obstacleGrid[-1][-1] == 1:
             return 0
-            
-        obstacleGrid[0][0] = -1
-        
+
+        dp = [[0] * col for _ in range(row)]
+
         for y in range(row):
             for x in range(col):
-                if obstacleGrid[y][x] != 1:
-                    if 0 < y < row and obstacleGrid[y-1][x] < 0:
-                        obstacleGrid[y][x] += obstacleGrid[y-1][x]
-                    if 0 < x < col and obstacleGrid[y][x-1] < 0:
-                        obstacleGrid[y][x] += obstacleGrid[y][x-1]
-        
-        return -obstacleGrid[-1][-1] if obstacleGrid[-1][-1] != 1 else 0
+                if obstacleGrid[y][x] == 1:
+                    continue
+
+                if y == 0 and x == 0:
+                    dp[y][x] = 1
+                elif y == 0:
+                    dp[y][x] = dp[y][x-1]
+                elif x == 0:
+                    dp[y][x] = dp[y-1][x]
+                else:
+                    dp[y][x] = dp[y][x-1] + dp[y-1][x]
+
+        return dp[-1][-1]

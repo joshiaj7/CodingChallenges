1ec711245e7652beb3cccd5540d77e7f152e822c
joshiaj7 <joshia.tan7@gmail.com>
2022-05-21 13:31:15 +0700

    leetcode 2022-05-21

--- python3/coin_change.py
+++ python3/coin_change.py
@@ -1,19 +1,19 @@
 """
 Space   : O(amount + 1)
 Time    : O((amount + 1) * coins)
+
+Method:
 DP approach
 """
 
 
 class Solution:
     def coinChange(self, coins: List[int], amount: int) -> int:
-        dp = [0] + [inf] * amount
-        n = amount + 1
+        dp = [0] + [float('inf')] * amount
         coins.sort()
 
         for c in coins:
-            for i in range(n):
-                if i - c >= 0:
-                    dp[i] = min(dp[i-c] + 1, dp[i])
+            for i in range(c, amount + 1):
+                dp[i] = min(dp[i], dp[i-c] + 1)
 
-        return dp[-1] if dp[-1] != inf else -1
+        return dp[-1] if dp[-1] != float('inf') else -1

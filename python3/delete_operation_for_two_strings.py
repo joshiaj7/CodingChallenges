dd89200d925e69ff730765b62734303aa06f8a83
joshiaj7 <joshia.tan7@gmail.com>
2022-06-14 20:50:08 +0700

    leetcode 2022-06-14

---
+++ python3/delete_operation_for_two_strings.py
@@ -0,0 +1,26 @@
+"""
+Space : O(n*m)
+Time  : O(n*m)
+
+Method:
+DP Approach
+bottom up
+Similar to 'longest common subsequence'
+"""
+
+class Solution:
+    def minDistance(self, word1: str, word2: str) -> int:
+        l1 = len(word1)
+        l2 = len(word2)
+        dp = [[0 for _ in range(l2+1)] for _ in range(l1+1)]
+        
+        
+        for i in range(l1-1, -1, -1):
+            for j in range(l2-1, -1, -1):
+                if word1[i] == word2[j]:
+                    dp[i][j] = dp[i+1][j+1] + 1
+                else:
+                    dp[i][j] = max(dp[i+1][j], dp[i][j+1])
+                    
+        
+        return l1 + l2 - 2 * dp[0][0]

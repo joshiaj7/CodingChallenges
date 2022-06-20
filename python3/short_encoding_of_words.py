8d182685aed6e9cc363a8c66e7e122a98d050601
joshiaj7 <joshia.tan7@gmail.com>
2022-06-20 20:16:21 +0700

    leetcode 2022-06-20

--- python3/short_encoding_of_words.py
+++ python3/short_encoding_of_words.py
@@ -1,15 +1,48 @@
-"""
-Space   : O(n)
-Time    : O(n**2)    
-"""
+class Trie:
+    def __init__(self):
+        self.d = {}
+        self.leaves = []
+
+    def insert(self, word):
+        p = self.d
+        for l in word:
+            if l not in p:
+                p[l] = {}
+            p = p[l]
+
+        # append leaf
+        if len(p) == 0:
+            self.leaves.append(word)
+
+    def getLeaves(self):
+        return self.leaves
 
 
 class Solution:
-    def minimumLengthEncoding(self, W: List[str]) -> int:
-        wset = set(W)
-        for word in W:
-            if word in wset:
+    def minimumLengthEncoding(self, words: List[str]) -> int:
+        """
+        Best solution
+        Space   : O(n)
+        Time    : O(n**2)    
+        """
+        word_set = set(words)
+        for word in words:
+            if word in word_set:
                 for i in range(1, len(word)):
-                    wset.discard(word[i:])
-            print(word, wset)
-        return len("#".join(list(wset))) + 1
+                    word_set.discard(word[i:])
+
+        return len("#".join(list(word_set))) + 1
+
+    def minimumLengthEncodingTrie(self, words: List[str]) -> int:
+        """
+        Space   : O(n)
+        Time    : O(n**2)   
+        """
+        trie = Trie()
+
+        words = list(set(words))
+        words.sort(key=lambda w: -len(w))
+        for word in words:
+            trie.insert(word[::-1])
+
+        return len("#".join(trie.getLeaves())) + 1

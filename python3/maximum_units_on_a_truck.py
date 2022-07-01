0b4dfe4f4a1a32fac274cc5330eff0c6e0fd8d99
joshiaj7 <joshia.tan7@gmail.com>
2022-07-01 15:08:27 +0700

    leetcode 2022-06-01

--- python3/maximum_units_on_a_truck.py
+++ python3/maximum_units_on_a_truck.py
@@ -1,20 +1,24 @@
-# Space : O(1)
-# Time  : O(n log n)
+"""
+Space : O(1)
+Time  : O(n log n)
+"""
+
 
 class Solution:
     def maximumUnits(self, boxTypes: List[List[int]], truckSize: int) -> int:
         ans = 0
+        cap = 0
+        boxTypes.sort(key=lambda b: -b[1])
         
-        boxTypes.sort(key=lambda x:x[1], reverse=True)
+        for q, v in boxTypes:
+            if q + cap <= truckSize:
+                cap += q
+                ans += (q * v)
+            else:
+                space = truckSize - cap
+                cap += space
+                ans += space * v
+            if cap == truckSize:
+                break 
         
-        for num, size in boxTypes:
-            if truckSize - num >= 0:
-                truckSize -= num
-                ans += num * size
-            elif truckSize - num < 0:
-                ans += truckSize * size
-                truckSize = 0
-            if truckSize == 0:
-                break
-            
         return ans

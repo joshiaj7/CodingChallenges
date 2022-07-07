1a25506372babfff1ef8a882f044c9f8abe9d95d
joshiaj7 <joshia.tan7@gmail.com>
2022-07-07 16:27:36 +0700

    fix ambiguity

--- python3/interleaving_string.py
+++ python3/interleaving_string.py
@@ -18,11 +18,11 @@ class Solution:
                 if i == 0 and j == 0:
                     dp[i][j] = True
                 elif i == 0:
-                    dp[i][j] = dp[i][j-1] and s2[j-1] == s3[i+j-1]
+                    dp[i][j] = dp[i][j-1] and (s2[j-1] == s3[i+j-1])
                 elif j == 0:
-                    dp[i][j] = dp[i-1][j] and s1[i-1] == s3[i+j-1]
+                    dp[i][j] = dp[i-1][j] and (s1[i-1] == s3[i+j-1])
                 else:
-                    dp[i][j] = (dp[i][j-1] and s2[j-1] == s3[i+j-1]
-                                ) or (dp[i-1][j] and s1[i-1] == s3[i+j-1])
+                    dp[i][j] = (dp[i][j-1] and (s2[j-1] == s3[i+j-1])
+                                ) or (dp[i-1][j] and (s1[i-1] == s3[i+j-1]))
 
         return dp[l1][l2]

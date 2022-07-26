71fcd1943e2fbfd38f2543232ffb02dbfe513dff
joshiaj7 <joshia.tan7@gmail.com>
2022-07-26 15:39:35 +0700

    leetcode 2022-07-26

---
+++ python3/lowest_common_ancestor_of_a_binary_tree.py
@@ -0,0 +1,17 @@
+from .model import TreeNode
+
+"""
+Space   : O(1)
+Time    : O(n)
+"""
+
+class Solution:
+    def lowestCommonAncestor(self, root: TreeNode, p: TreeNode, q: TreeNode) -> TreeNode:
+        if root in [None, p, q]:
+            return root
+        
+        left = self.lowestCommonAncestor(root.left, p, q)
+        right = self.lowestCommonAncestor(root.right, p, q)
+        
+        return root if left and right else left or right
+            

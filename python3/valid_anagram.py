3efb68128aa2f0dee2f3ebbc7dbba94d56ba174d
joshiaj7 <joshia.tan7@gmail.com>
2022-07-28 17:02:34 +0700

    leetcode 2022-07-28

--- python3/valid_anagram.py
+++ python3/valid_anagram.py
@@ -1,8 +1,22 @@
-# leetcode
+"""
+Space   : O(n)
+Time    : O(n)
+"""
 
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        s = sorted(s)
-        t = sorted(t)
-                
-        return s == t
+        sd, td = {}, {}
+        
+        for l in s:
+            if l in sd:
+                sd[l] += 1
+            else:
+                sd[l] = 1
+        
+        for l in t:
+            if l in td:
+                td[l] += 1
+            else:
+                td[l] = 1
+        
+        return sd == td

923baabef1fda373dd4a414cc31ad6ddab8357de
joshiaj7 <joshia.tan7@gmail.com>
2022-08-23 19:15:52 +0700

    leetcode 2022-08-23

--- python3/palindrome_linked_list.py
+++ python3/palindrome_linked_list.py
@@ -1,23 +1,18 @@
 from .model import ListNode
 
-# Space : O(n) 
+# Space : O(n)
 # Time  : O(n)
+
 
 class Solution:
     def isPalindrome(self, head: ListNode) -> bool:
-            mem = []
-
-            p = head
-            while p:
-                mem.append(p.val)
-                p = p.next
-                
-            s, e = 0, len(mem)-1
-            while s < e:
-                if mem[s] != mem[e]:
-                    return False
-                s += 1
-                e -= 1
-            
-                
+        if not head:
             return True
+
+        temp = []
+        p = head
+        while p:
+            temp.append(p.val)
+            p = p.next
+
+        return temp == temp[::-1]

73f640248fdff048abb101591c51f9edb384f40e
joshiaj7 <joshia.tan7@gmail.com>
2022-11-07 19:46:57 +0700

    leetcode 2022-11-07

---
+++ python3/maximum_69_number.py
@@ -0,0 +1,16 @@
+"""
+Space   : O(n)
+Time    : O(n)
+"""
+
+
+class Solution:
+    def maximum69Number(self, num: int) -> int:
+        s = str(num)
+        n = len(s)
+
+        for i in range(n):
+            if s[i] == '6':
+                return int(s[:i] + '9' + s[i+1:])
+
+        return num

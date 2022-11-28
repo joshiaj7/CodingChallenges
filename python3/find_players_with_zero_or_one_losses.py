0f7eb2655ac9f6f51191adc572280dec71abfbcd
joshiaj7 <joshia.tan7@gmail.com>
2022-11-28 19:42:07 +0700

    leetcode 2022-11-28

---
+++ python3/find_players_with_zero_or_one_losses.py
@@ -0,0 +1,23 @@
+class Solution:
+    def findWinners(self, matches: List[List[int]]) -> List[List[int]]:
+        ans = [[], []]
+        lose = {}
+        players = set()
+
+        for w, l in matches:
+            if l in lose:
+                lose[l] += 1
+            else:
+                lose[l] = 1
+
+            players.add(w)
+            players.add(l)
+
+        players = sorted(list(players))
+        for p in players:
+            if p not in lose:
+                ans[0].append(p)
+            elif lose[p] == 1:
+                ans[1].append(p)
+
+        return ans

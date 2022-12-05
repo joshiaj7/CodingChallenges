88f9f728d0169420d4976cd9988e234f75989ccd
joshiaj7 <joshia.tan7@gmail.com>
2022-12-05 19:01:27 +0700

    leetcode 2022-12-05

--- python3/middle_of_the_linked_list.py
+++ python3/middle_of_the_linked_list.py
@@ -1,23 +1,18 @@
 from .model import ListNode
 
 """
-Space   : O(n)
+Space   : O(1)
 Time    : O(n)
 """
-
 class Solution:
     def middleNode(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        p1, p2 = head, head
-        
-        l = 0
-        while p1:
-            l += 1
-            p1 = p1.next
-            
-        l //= 2
-        while l > 0:
-            p2 = p2.next
-            l -= 1
-    
-        return p2
-        
+        slow, fast = head, head
+
+        while fast.next:
+            slow = slow.next
+            fast = fast.next
+
+            if fast and fast.next:
+                fast = fast.next
+
+        return slow

d4ba08b1a14e3af4d7ba9131568ff0b908ee992c
joshiaj7 <joshia.tan7@gmail.com>
2022-12-24 13:21:57 +0700

    leetcode 2022-12-24

---
+++ python3/domino_and_tromino_tiling.py
@@ -0,0 +1,18 @@
+"""
+Space   : O(n)
+Time    : O(n)
+"""
+
+
+class Solution:
+    def numTilings(self, n: int) -> int:
+        dp = [1, 2, 5]
+        
+        if n <= 3:
+            return dp[n-1]
+
+        for i in range(3, n):
+            dp.append(2 * dp[i-1] + dp[i-3]) 
+
+        return dp[n-1] % (10 ** 9  + 7)
+

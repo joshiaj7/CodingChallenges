e5df35e62e2c7a0670172f2950fc587c16ff989b
joshiaj7 <joshia.tan7@gmail.com>
2023-02-06 12:55:11 +0700

    leetcode 2023-02-06

---
+++ python3/shuffle_the_array.py
@@ -0,0 +1,14 @@
+"""
+Space   : O(2n)
+Time    : O(n)
+"""
+
+class Solution:
+    def shuffle(self, nums: List[int], n: int) -> List[int]:
+        ans = []
+
+        for i in range(n):
+            ans.append(nums[i])
+            ans.append(nums[i+n])
+
+        return ans

9fa080ea03738612f41d44f208c0ef5cf4a7d96c
joshiaj7 <joshia.tan7@gmail.com>
2023-02-16 08:57:57 +0700

    leetcode 2023-02-16

--- python3/maximum_depth_of_binary_tree.py
+++ python3/maximum_depth_of_binary_tree.py
@@ -1,11 +1,5 @@
 from .model import TreeNode
 
-"""
-Space   : O(1)
-Time    : O(n)
-"""
-
-
 class Solution:
     def getDepth(self, root: TreeNode) -> int:
         if not root:
@@ -16,5 +10,34 @@ class Solution:
 
         return max(l, r)
 
-    def maxDepth(self, root: TreeNode) -> int:
+    def maxDepthDFS(self, root: TreeNode) -> int:
+        """
+        Space   : O(1)
+        Time    : O(n)
+        """
         return self.getDepth(root)
+
+    def maxDepthBFS(self, root: Optional[TreeNode]) -> int:
+        """
+        Space   : O(n)
+        Time    : O(n)
+        """
+
+        if not root:
+            return 0
+        
+        ans = 0
+        stack = [root]
+
+        while stack:
+            temp = []
+            for node in stack:
+                if node.left:
+                    temp.append(node.left)
+                if node.right:
+                    temp.append(node.right)
+
+            ans += 1
+            stack = temp
+
+        return ans

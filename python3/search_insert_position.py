50d1577b7e4064d11659832bb6798f45ab54adba
joshiaj7 <joshia.tan7@gmail.com>
2023-02-20 09:31:20 +0700

    leetcode 2023-02-20

--- python3/search_insert_position.py
+++ python3/search_insert_position.py
@@ -1,15 +1,18 @@
-# leetcode
+"""
+Space   : O(1)
+Time    : O(log n)
+"""
 
 class Solution:
     def searchInsert(self, nums: List[int], target: int) -> int:
-        ans = 0
-        leng = len(nums)
-        
-        for idx in range(leng):
-            if nums[idx] == target:
-                break
-            elif nums[idx] > target:
-                break
-            ans += 1
-                
-        return ans
+        n = len(nums)
+
+        left, right = 0, n-1
+        while left <= right:
+            mid = (left + right) // 2
+            if nums[mid] < target:
+                left = mid + 1
+            else:
+                right = mid - 1
+
+        return left

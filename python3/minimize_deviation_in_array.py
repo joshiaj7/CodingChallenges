1deb41a6c13d8a1215091ed2d1d6b8c30b54a51f
joshiaj7 <joshia.tan7@gmail.com>
2023-02-24 10:40:47 +0700

    leetcode 2023-02-24

--- python3/minimize_deviation_in_array.py
+++ python3/minimize_deviation_in_array.py
@@ -1,22 +1,32 @@
-import heapq
+from heapq import heappop, heappush, heapify
 
 """
-Best solution
 Space   : O(n)
 Time    : O(n (log n) (log m))
-where n is number of nums and m = max(nums)
+n == len(nums)
+m == max(nums)
 """
 
 
 class Solution:
     def minimumDeviation(self, nums: List[int]) -> int:
-        heap = list(set(-(x * 2 if x & 1 else x) for x in nums))
-        heapq.heapify(heap)
-        ma, mi = heap[0], max(heap)
+        for i in range(len(nums)):
+            if nums[i] % 2 == 1:
+                nums[i] = nums[i] * -2
+            else:
+                nums[i] *= -1
+
+        heap = list(set(nums))
+        heapify(heap) 
+
+        ma, mi = heap[0], max(heap) 
         ans = ma - mi
+
         while ma % 2 == 0:
             x = ma // 2
-            heapq.heapreplace(heap, x)
+            heappop(heap)
+            heappush(heap, x)
             ma, mi = heap[0], max(mi, x)
             ans = max(ans, ma - mi)
+
         return -ans

a0f99f4295fbc3f033fbe94a23c8a32161ac07b6
joshiaj7 <joshia.tan7@gmail.com>
2023-02-26 14:10:53 +0700

    leetcode 2023-02-26

---
+++ python3/edit_distance.py
@@ -0,0 +1,30 @@
+"""
+Space   : O(n + m)
+Time    : O(n * m)
+Algorithm : Levenshtein distance
+"""
+
+class Solution:
+    def minDistance(self, word1: str, word2: str) -> int:
+        n, m  = len(word1), len(word2)
+        dp = [[0 for _ in range(m+1)] for _ in range(n+1)]
+
+        for i in range(n+1):
+            dp[i][0] = i
+
+        for j in range(m+1):
+            dp[0][j] = j
+
+        for i in range(1, n+1):
+            for j in range(1, m+1):
+                if word1[i-1] == word2[j-1]:
+                    dp[i][j] = dp[i-1][j-1]
+                else:
+                    insertion = 1 + dp[i][j-1]
+                    deletion = 1 + dp[i-1][j]
+                    replacement = 1 + dp[i-1][j-1]
+
+                    dp[i][j] = min(insertion, deletion, replacement)
+                    
+        return dp[n][m]
+

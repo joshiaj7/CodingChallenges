71d2e0a5da0e91f0f5e6861917cf5fc96a33a7a8
joshiaj7 <joshia.tan7@gmail.com>
2023-03-12 18:55:46 +0700

    leetcode 2023-03-12

---
+++ golang/merge_k_sorted_lists.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"golang/model"
+	"sort"
+)
+
+func mergeKLists(lists []*model.ListNode) *model.ListNode {
+	temp := []int{}
+	ans := &model.ListNode{Val: 0, Next: nil}
+	k := len(lists)
+	p := &model.ListNode{}
+
+	for i := 0; i < k; i++ {
+		p = lists[i]
+		for p != nil {
+			temp = append(temp, p.Val)
+			p = p.Next
+		}
+	}
+
+	sort.Ints(temp)
+	p = ans
+	for _, val := range temp {
+		p.Next = &model.ListNode{Val: val, Next: nil}
+		p = p.Next
+	}
+
+	return ans.Next
+}

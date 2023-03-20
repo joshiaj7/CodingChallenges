125867b0b0bb353bd73bf9fea5610eb97b59fa9f
joshiaj7 <joshia.tan7@gmail.com>
2023-03-20 09:45:23 +0700

    leetcode 2023-03-20

--- python3/can_place_flowers.py
+++ python3/can_place_flowers.py
@@ -1,61 +1,34 @@
 """
-Space   : O(n)
+Space   : O(1)
 Time    : O(n)
 """
 
-
 class Solution:
     def canPlaceFlowers(self, flowerbed: List[int], n: int) -> bool:
-        room1, room2 = 0, 0
+        count, place = 0, 0
+        all_zero = True
+        beginning = flowerbed[0] == 0
         leng = len(flowerbed)
-        pos1, pos2 = flowerbed.copy(), flowerbed.copy()
 
         if n == 0:
             return True
 
-        if leng == 1:
-            if flowerbed[0] == 0:
-                return True
+        for i in range(leng):
+            if flowerbed[i] == 0:
+                count += 1
             else:
-                return False
+                all_zero = False
+                if beginning:
+                    place += count // 2
+                    beginning = False
+                elif count > 0:
+                    place += (count - 1) // 2
+                count = 0
 
-        i = 0
-        while i < leng:
-            if i == 0:
-                # forward
-                if pos1[i] == 0 and pos1[i+1] == 0:
-                    pos1[i] = 1
-                    room1 += 1
+        if all_zero:
+            return (leng+1) // 2 >= n
+                
+        if count > 0:
+            place += count // 2
 
-                # backward
-                if pos2[leng-i-1] == 0 and pos2[leng-i-2] == 0:
-                    pos2[leng-i-1] = 1
-                    room2 += 1
-
-            elif i < leng-1:
-                # forward
-                if pos1[i] == 0 and pos1[i-1] == 0 and pos1[i+1] == 0:
-                    pos1[i] = 1
-                    room1 += 1
-
-                # backward
-                if pos2[leng-i-1] == 0 and pos2[leng-i-2] == 0 and pos2[leng-i] == 0:
-                    pos2[leng-i-1] = 1
-                    room2 += 1
-
-            elif i == leng-1:
-                # forward
-                if pos1[i] == 0 and pos1[i-1] == 0:
-                    pos1[i] = 1
-                    room1 += 1
-                # backward
-                if pos2[leng-i-1] == 0 and pos2[leng-i] == 0:
-                    pos2[leng-i-1] = 1
-                    room2 += 1
-
-            i += 1
-
-        if n <= max(room1, room2):
-            return True
-
-        return False
+        return place >= n

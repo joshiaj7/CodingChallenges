d33b02da5bd1da70a2b5029af4bb38dcb0c584a4
joshiaj7 <joshia.tan7@gmail.com>
2023-03-21 08:44:54 +0700

    leetcode 2023-03-21

---
+++ python3/number_of_zero_filled_subarrays.py
@@ -0,0 +1,18 @@
+"""
+Space   : O(1)
+Time    : O(n)
+"""
+
+class Solution:
+    def zeroFilledSubarray(self, nums: List[int]) -> int:
+        ans = 0 
+        count = 0
+
+        for x in nums:
+            if x == 0:
+                count += 1
+                ans += count 
+            else:
+                count = 0
+
+        return ans

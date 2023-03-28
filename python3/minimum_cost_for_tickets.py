1af9a41340350562fbde0f5bb5c2f4696271fc49
joshiaj7 <joshia.tan7@gmail.com>
2023-03-28 09:56:25 +0700

    leetcode 2023-03-28

---
+++ python3/minimum_cost_for_tickets.py
@@ -0,0 +1,31 @@
+"""
+Space   : O(n)
+Time    : O(n)
+n = biggest day
+"""
+
+
+class Solution:
+    def mincostTickets(self, days: List[int], costs: List[int]) -> int:
+        n = days[-1] + 1
+        dp = [0] * n
+
+        j = 0
+        for i in range(n):
+            days1 = float('inf')
+            days7 = float('inf')
+            days30 = float('inf')
+            if i == days[j]:
+
+                days1 = dp[i-1] + costs[0]
+                days7 = dp[max(i-7, 0)] + costs[1]
+                days30 = dp[max(i-30, 0)] + costs[2]
+
+                dp[i] = min(days1, days7, days30)
+                j += 1
+            elif i > 0:
+                dp[i] = dp[i-1]
+            
+
+
+        return dp[-1]

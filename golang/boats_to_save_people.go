20b82139f866edc8b8a644c3d28621a847b1b5e0
joshiaj7 <joshia.tan7@gmail.com>
2023-04-03 10:27:53 +0700

    leetcode 2023-04-03

---
+++ golang/boats_to_save_people.go
@@ -0,0 +1,20 @@
+package main
+
+import "sort"
+
+func numRescueBoats(people []int, limit int) int {
+	ans := 0
+	sort.Ints(people)
+	n := len(people)
+	start, end := 0, n-1
+
+	for start <= end {
+		if people[start]+people[end] <= limit {
+			start++
+		}
+		end--
+		ans++
+	}
+
+	return ans
+}

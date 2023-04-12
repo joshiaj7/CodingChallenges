407b7e59d5d65db91756b4fabe91ceabc01800ba
joshiaj7 <joshia.tan7@gmail.com>
2023-04-12 09:52:18 +0700

    leetcode 2023-04-12

--- python3/simplify_path.py
+++ python3/simplify_path.py
@@ -6,21 +6,17 @@ Time    : O(n)
 
 class Solution:
     def simplifyPath(self, path: str) -> str:
-        paths = path.split('/')
-        i = 0
-
-        while i < len(paths):
-            if paths[i] == '' or paths[i] == '.':
-                del paths[i]
-            else:
-                i += 1
-
         stack = []
-        for i in paths:
-            if i == '..':
+        splitted_path = path.split("/")
+
+        for directory in splitted_path:
+            if directory == "" or directory == ".":
+                continue
+
+            if directory == "..": 
                 if len(stack) > 0:
                     stack.pop()
             else:
-                stack.append(i)
+                stack.append(directory)
 
         return "/" + "/".join(stack)

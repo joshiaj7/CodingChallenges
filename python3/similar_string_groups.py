42a0157698e636cbcfe86a90779cb6d042dfb0d0
joshiaj7 <joshia.tan7@gmail.com>
2023-04-28 10:14:15 +0700

    leetcode 2023-04-28

---
+++ python3/similar_string_groups.py
@@ -0,0 +1,40 @@
+"""
+Space   : O(n)
+Time    : O(nm)
+
+Union Find
+"""
+
+class Solution:
+    def numSimilarGroups(self, strs: List[str]) -> int:
+        n = len(strs)
+        m = len(strs[0])
+        uf = UnionFind(n)
+        for i in range(n):
+            for j in range(i+1, n):
+                if sum(strs[i][k] != strs[j][k] for k in range(m)) in (0, 2):
+                    uf.union(i, j)
+        return len(set(uf.find(i) for i in range(n)))
+
+
+class UnionFind:
+    def __init__(self, n):
+        self.parent = list(range(n))
+        self.rank = [0 for _ in range(n)]
+
+    def find(self, x):
+        while x != self.parent[x]:
+            x = self.parent[x]
+        return x
+
+    def union(self, x, y):
+        root1 = self.find(x)
+        root2 = self.find(y)
+        if root1 == root2:
+            return
+        if self.rank[root1] > self.rank[root2]:
+            self.parent[root2] = root1
+        else:
+            self.parent[root1] = root2
+            if self.rank[root1] == self.rank[root2]:
+                self.rank[root2] += 1

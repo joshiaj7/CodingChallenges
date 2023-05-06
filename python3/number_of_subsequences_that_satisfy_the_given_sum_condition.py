cbfc6eab66e48afcdcc80a2064f2f61ca7211f0e
joshiaj7 <joshia.tan7@gmail.com>
2023-05-06 13:35:22 +0700

    leetcode 2023-05-06

---
+++ python3/number_of_subsequences_that_satisfy_the_given_sum_condition.py
@@ -0,0 +1,20 @@
+"""
+Space   : O(1)
+Time    : O(n log n)
+"""
+
+class Solution:
+    def numSubseq(self, nums: List[int], target: int) -> int:
+        ans = 0
+        mod = 10 ** 9 + 7
+        l, r = 0, len(nums)-1
+        nums.sort()
+
+        while l <= r:
+            if nums[l] + nums[r] > target:
+                r -= 1
+            else:
+                ans += pow(2, (r - l), mod)
+                l += 1
+
+        return ans % mod

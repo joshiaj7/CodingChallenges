45661c9cb2b34f0355ceebde70321e94456d00eb
joshiaj7 <joshia.tan7@gmail.com>
2023-05-08 09:04:27 +0700

    leetcode 2023-05-08

---
+++ python3/matrix_diagonal_sum.py
@@ -0,0 +1,12 @@
+class Solution:
+    def diagonalSum(self, mat: List[List[int]]) -> int:
+        ans = 0
+        n = len(mat)
+
+        for i in range(n):
+            for j in range(n):
+                if i - j == 0 or i + j == n - 1:
+                    ans += mat[i][j]
+                
+        return ans
+    

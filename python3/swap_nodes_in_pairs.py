5f68ec4a1148ab949c4a5a30b8a040f61021815a
joshiaj7 <joshia.tan7@gmail.com>
2023-05-16 09:32:35 +0700

    leetcode 2023-05-16

--- python3/swap_nodes_in_pairs.py
+++ python3/swap_nodes_in_pairs.py
@@ -1,22 +1,40 @@
 from .model import ListNode
 
 """
-Space   : O(1)
-Time    : O(n/2)
+Space   : O(n)
+Time    : O(n)
 """
 
 class Solution:
-    def swapPairs(self, head: ListNode) -> ListNode:
-        point = head
+    def swapPairs(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        if not head:
+            return None
 
-        while point:
-            temp = point.val
+        if not head.next:
+            return head
+    
+        ans = ListNode()
+        p = ans
 
-            if not point.next:
+        while True:
+            p1, p2 = head, head.next
+            if p1 and p2:
+                tmp = p2.next
+
+                p1.next = None
+                p2.next = None
+
+                p.next = p2
+                p.next.next = p1
+                
+                head = tmp
+                p = p.next.next
+            elif p1 and not p2:
+                p.next = p1
                 break
-            point.val = point.next.val
-            point.next.val = temp
+            
+            if not head:
+                break
 
-            point = point.next.next
 
-        return head
+        return ans.next

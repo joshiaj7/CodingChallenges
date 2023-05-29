b074bf580b4a0e10572328a1343cd179c3c9a032
joshiaj7 <joshia.tan7@gmail.com>
2023-05-29 09:38:46 +0700

    leetcode 2023-05-29

---
+++ python3/design_parking_system.py
@@ -0,0 +1,20 @@
+"""
+Space   : O(1)
+Time    : O(n)
+"""
+
+class ParkingSystem:
+
+    def __init__(self, big: int, medium: int, small: int):
+        self.hashmap = {
+            1: big,
+            2: medium,
+            3: small,
+        }
+
+    def addCar(self, carType: int) -> bool:
+        if self.hashmap[carType] > 0:
+            self.hashmap[carType] -= 1
+            return True
+        
+        return False

0efe4ceb07db848b8d8194fad69b26ed011b74b5
joshiaj7 <joshia.tan7@gmail.com>
2023-06-22 09:46:10 +0700

    leetcode 2023-06-22

---
+++ python3/best_time_to_buy_and_sell_stock_with_transaction_fee.py
@@ -0,0 +1,23 @@
+from typing import List
+
+"""
+Space   : O(1)
+Time    : O(n)
+"""
+
+class Solution:
+    def maxProfit(self, prices: List[int], fee: int) -> int:
+        n = len(prices)
+        if n < 2:
+            return 0
+
+        ans = 0
+        minimum = prices[0]
+        for i in range(1, n):
+            if prices[i] < minimum:
+                minimum = prices[i]
+            elif prices[i] > minimum + fee:
+                ans += prices[i] - fee - minimum
+                minimum = prices[i] - fee
+
+        return ans

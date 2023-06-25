4e4c3fe73728cdb1c2edffb8e5540f7648f82788
joshiaj7 <joshia.tan7@gmail.com>
2023-06-25 14:53:56 +0700

    recursion mode

--- python3/count_all_possible_routes.py
+++ python3/count_all_possible_routes.py
@@ -1,4 +1,5 @@
 from typing import List
+from functools import lru_cache
 
 """
 Space   : O(n * fuel)
@@ -21,3 +22,26 @@ class Solution:
                         dp[city][f] = (dp[city][f] + dp[next_city][f - abs(locations[next_city] - locations[city])]) % MOD
         
         return dp[start][fuel]
+
+    def countRoutesRecurse(self, locations: List[int], start: int, finish: int, fuel: int) -> int:
+        N = len(locations)
+        MOD = 10**9 +7
+        
+        @lru_cache
+        def recur(i, fuel):
+            if fuel < 0:
+                return 0
+            
+            ans = 0 
+            if i == finish:
+                ans += 1
+            
+            for j in range(N):
+                if j == i:
+                    continue
+                ans += recur(j, fuel - abs(locations[i] - locations[j]))
+            
+            return ans % MOD
+        
+        
+        return recur(start, fuel)

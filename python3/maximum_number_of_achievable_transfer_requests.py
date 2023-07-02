0764673a2f627796e5264c23986f3754d638ae7b
joshiaj7 <joshia.tan7@gmail.com>
2023-07-02 14:46:12 +0700

    leetcode 2023-07-02

--- python3/maximum_number_of_achievable_transfer_requests.py
+++ python3/maximum_number_of_achievable_transfer_requests.py
@@ -7,6 +7,7 @@ Time    : O(2^r * (n + r))
 
 
 class Solution:
+    # masking
     def maximumRequests(self, n: int, requests: List[List[int]]) -> int:
         r = len(requests)
         ans = 0
@@ -26,3 +27,28 @@ class Solution:
             ans = max(ans, brute_force(i))
 
         return ans
+
+    # backtrack
+    def helper(self, start, requests, indegree, n, count):
+        if start == len(requests):
+            for i in range(n):
+                if indegree[i] != 0:
+                    return
+            self.ans = max(self.ans, count)
+            return
+
+        # Take 
+        indegree[requests[start][0]] -= 1
+        indegree[requests[start][1]] += 1
+        self.helper(start + 1, requests, indegree, n, count + 1)
+
+        # Not-take
+        indegree[requests[start][0]] += 1
+        indegree[requests[start][1]] -= 1
+        self.helper(start + 1, requests, indegree, n, count)
+
+    def maximumRequestsBacktrack(self, n, requests):
+        self.ans = 0
+        indegree = [0] * n
+        self.helper(0, requests, indegree, n, 0)
+        return self.ans

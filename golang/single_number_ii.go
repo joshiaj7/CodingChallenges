2ed7cd9b48225f61363b8138240309eeb0d76a78
joshiaj7 <joshia.tan7@gmail.com>
2023-07-04 09:37:31 +0700

    leetcode 2023-07-04

---
+++ golang/single_number_ii.go
@@ -0,0 +1,18 @@
+package main
+
+/*
+Space	: O(1)
+Time	: O(n)
+*/
+
+func singleNumber(nums []int) int {
+	ones, twos := 0, 0
+	n := len(nums)
+
+	for i := 0; i < n; i++ {
+		ones = (ones ^ nums[i]) & (^twos)
+		twos = (twos ^ nums[i]) & (^ones)
+	}
+
+	return ones
+}

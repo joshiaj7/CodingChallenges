24273eedd5e593b873615f5b4206124f643e1198
joshiaj7 <joshia.tan7@gmail.com>
2023-07-27 09:00:11 +0700

    leetcode 2023-07-27

---
+++ python3/maximum_running_time_of_n_computers.py
@@ -0,0 +1,18 @@
+from typing import List
+
+"""
+Space   : O(1)
+Time    : O(m log m)
+"""
+
+
+class Solution:
+    def maxRunTime(self, n: int, batteries: List[int]) -> int:
+        batteries.sort()
+        su = sum(batteries)
+        
+        while batteries[-1] > su // n:
+            n -= 1
+            su -= batteries.pop()
+
+        return su // n

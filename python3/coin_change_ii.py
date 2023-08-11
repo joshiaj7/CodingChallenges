7f2f23f556dedc3b7ae0d31c033cb0f731dc5078
joshiaj7 <joshia.tan7@gmail.com>
2023-08-11 11:06:08 +0700

    leetcode 2023-08-11

---
+++ python3/coin_change_ii.py
@@ -0,0 +1,19 @@
+from typing import List
+
+"""
+Space   : O(n)
+Time    : O(c * amount)
+"""
+
+
+class Solution:
+    def change(self, amount: int, coins: List[int]) -> int:
+        dp = [0] * (amount + 1)
+        dp[0] = 1
+
+        for i in coins:
+            for j in range(1, amount + 1):
+                if j >= i:
+                    dp[j] += dp[j - i]
+    
+        return dp[amount]

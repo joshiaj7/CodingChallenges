09156c03ab3a5fe131802bc00ebcf25ed6661b6a
joshiaj7 <joshia.tan7@gmail.com>
2023-08-12 20:11:43 +0700

    leetcode 2023-08-12

--- python3/unique_paths_II.py
+++ python3/unique_paths_II.py
@@ -9,26 +9,24 @@ Dynamic Programming (DP)
 
 class Solution:
     def uniquePathsWithObstacles(self, obstacleGrid: List[List[int]]) -> int:
-        row = len(obstacleGrid)
-        col = len(obstacleGrid[0])
+        m, n = len(obstacleGrid), len(obstacleGrid[0])
 
-        if obstacleGrid[0][0] == 1 or obstacleGrid[-1][-1] == 1:
-            return 0
-
-        dp = [[0] * col for _ in range(row)]
-
-        for y in range(row):
-            for x in range(col):
-                if obstacleGrid[y][x] == 1:
+        for i in range(m):
+            for j in range(n):
+                if obstacleGrid[i][j] != 1:
                     continue
 
-                if y == 0 and x == 0:
-                    dp[y][x] = 1
-                elif y == 0:
-                    dp[y][x] = dp[y][x-1]
-                elif x == 0:
-                    dp[y][x] = dp[y-1][x]
+                if i == 0 and j == 0:
+                    obstacleGrid[i][j] = -1
+                elif i == 0:
+                    obstacleGrid[i][j] = obstacleGrid[i][j-1]
+                elif j == 0:
+                    obstacleGrid[i][j] = obstacleGrid[i-1][j]
                 else:
-                    dp[y][x] = dp[y][x-1] + dp[y-1][x]
+                    if obstacleGrid[i][j-1] != 1:
+                        obstacleGrid[i][j] += obstacleGrid[i][j-1] 
+                    if obstacleGrid[i-1][j] != 1:
+                        obstacleGrid[i][j] += obstacleGrid[i-1][j] 
 
-        return dp[-1][-1]
+        return -obstacleGrid[-1][-1] if obstacleGrid[-1][-1] != 1 else 0
+

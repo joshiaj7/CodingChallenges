2d9956da64452e52ae53beffc4acea26b402e3d2
joshiaj7 <joshia.tan7@gmail.com>
2023-08-18 10:36:01 +0700

    leetcode 2023-08-18

---
+++ python3/maximal_network_rank.py
@@ -0,0 +1,25 @@
+from typing import List
+from collections import defaultdict
+
+"""
+Space   : O(n^2)
+Time    : O(n^2)
+"""
+
+class Solution:
+    def maximalNetworkRank(self, n: int, roads: List[List[int]]) -> int:
+        ans = 0
+        graph = defaultdict(set)
+
+        for u, v in roads:
+            graph[u].add(v)
+            graph[v].add(u)
+
+        for u in range(n):
+            for v in range(u+1, n):
+                if v in graph[u]:
+                    ans = max(ans, len(graph[u]) + len(graph[v]) - 1)
+                else:
+                    ans = max(ans, len(graph[u]) + len(graph[v]))
+
+        return ans

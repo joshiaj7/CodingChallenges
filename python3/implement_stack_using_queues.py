c718a477954725d3ba64a93f9957e03656a3e8a9
joshiaj7 <joshia.tan7@gmail.com>
2023-08-28 10:17:16 +0700

    leetcode 2023-08-28

--- python3/implement_stack_using_queues.py
+++ python3/implement_stack_using_queues.py
@@ -1,23 +1,27 @@
 from collections import deque
 
-# Space     : O(n)
-# Push Time : O(1)
-# Pop Time  : O(1)
-
+"""
+Space     : O(n)
+Push Time : O(n)
+Pop Time  : O(1)
+"""
 
 class MyStack:
 
     def __init__(self):
-        self.dq = deque()
+        self.queue = deque()
 
     def push(self, x: int) -> None:
-        self.dq.appendleft(x)
+        temp = deque([x])
+        temp.extend(self.queue)
+        self.queue = temp
 
     def pop(self) -> int:
-        return self.dq.popleft()
+        return self.queue.popleft()
 
     def top(self) -> int:
-        return self.dq[0]
+        return self.queue[0]
 
     def empty(self) -> bool:
-        return len(self.dq) == 0
+        return len(self.queue) == 0
+        

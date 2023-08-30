baac8cf9710bc97ac4d613daaa377821f6817bc4
joshiaj7 <joshia.tan7@gmail.com>
2023-08-30 09:32:34 +0700

    leetcode 2023-08-30

--- python3/minimum_replacements_to_sort_the_array.py
+++ python3/minimum_replacements_to_sort_the_array.py
@@ -7,13 +7,43 @@ Google interview question
 
 class Solution:
     def minimumReplacement(self, nums: List[int]) -> int:
-        x = nums[-1]
+        last = nums[-1] # current biggest number
         n = len(nums)
         ans = 0
 
+        """
+        initial 
+        nums = [2, 10, 5]
+        x = 5
+
+        1st iter
+        cur = 5
+        k = (5 + 5 - 1) // 5 = 1
+        x = 5 // 1 = 5
+        ans += 1 - 1 = 0
+
+        2nd iter
+        cur = 10
+        k = (10 + 5 - 1) // 5 = 2
+        x = 10 // 2 = 5
+        ans += 2 - 1 = 1
+
+        3rd iter
+        cur = 2
+        k = (2 + 5 - 1) // 5 = 1
+        x = 2 // 1 = 2
+        ans += 1 - 1 = 1
+        """
+
         for i in range(n -1, -1, -1):
-            k = (nums[i] + x - 1) // x
-            x = nums[i] // k
+            # ceiling division
+            k = (nums[i] + last - 1) // last
+
+            # use ceiling division to get the smallest number
+            # and store it in x
+            last= nums[i] // k
+
+            # add answer - 1
             ans += k - 1
             
         return ans

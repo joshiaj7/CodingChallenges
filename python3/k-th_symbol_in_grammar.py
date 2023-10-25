862bd057be6a865b2506bcc316f592ab05d4cdb4
joshiaj7 <joshia.tan7@gmail.com>
2023-10-25 09:13:06 +0700

    leetcode 2023-10-25

---
+++ python3/k-th_symbol_in_grammar.py
@@ -0,0 +1,15 @@
+"""
+Space   : O(n)
+Time    : O(n)
+"""
+
+class Solution:
+    def kthGrammar(self, n: int, k: int) -> int:
+        if n == 1:
+            return 0
+
+        length = 2 ** (n - 2)
+        if k <= length:
+            return self.kthGrammar(n-1, k)
+
+        return 1 - self.kthGrammar(n-1, k-length)

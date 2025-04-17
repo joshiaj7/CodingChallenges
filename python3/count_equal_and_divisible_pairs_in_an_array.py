3559fd8b7d137b96e838a749c4ea3ccf0d3afa8d
joshiaj7 <joshia.tan7@gmail.com>
2025-04-17 13:50:52 +0700

    leetcode 2025-04-17

---
+++ python3/count_equal_and_divisible_pairs_in_an_array.py
@@ -0,0 +1,27 @@
+from collections import defaultdict
+
+"""
+Space   : O(n)  
+Time    : O(n^2)
+"""
+
+class Solution:
+    def countPairs(self, nums: List[int], k: int) -> int:
+        ans = 0
+
+        d = defaultdict(list)
+        for i, v in enumerate(nums):
+            d[v].append(i)
+
+        for key, val in d.items():
+            if len(val) <= 1:
+                continue
+
+            m = len(val)
+            for i in range(m):
+                for j in range(i+1, m):
+                    if (val[i] * val[j]) % k == 0:
+                        ans += 1
+
+        return ans
+        

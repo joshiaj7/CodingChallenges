3263d8624047d02fb6b6afe31f406b966538caa1
joshiaj7 <joshia.tan7@gmail.com>
2025-05-27 13:53:43 +0700

    leetcode 2025-05-27

---
+++ python3/divisible_and_non_divisible_sums_difference.py
@@ -0,0 +1,18 @@
+""""
+Space   : O(1)
+Time    : O(n)
+"""
+
+class Solution:
+    def differenceOfSums(self, n: int, m: int) -> int:
+        num1 = 0
+        num2 = 0
+
+        for i in range(1, n+1):
+            if i % m != 0:
+                num1 += i
+            else:
+                num2 += i
+
+        return num1 - num2
+        

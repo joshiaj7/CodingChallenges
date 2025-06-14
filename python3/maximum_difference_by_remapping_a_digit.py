05a575afda2056b664cf181a6192ebbe7e57d459
joshiaj7 <joshia.tan7@gmail.com>
2025-06-14 13:31:00 +0700

    leetcode 2025-06-14

---
+++ python3/maximum_difference_by_remapping_a_digit.py
@@ -0,0 +1,37 @@
+class Solution:
+    def minMaxDifference(self, num: int) -> int:
+        biggest = 0
+        smallest = 0
+
+        s = str(num)
+
+        # get biggest
+        big = ""
+        for l in s:
+            if l == "9":
+                continue 
+            big = l
+            break
+        
+        if big == "":
+            biggest = num
+        else:
+            temp = ""
+            for l in s:
+                if l == big:
+                    temp += "9"
+                else:
+                    temp += l
+            biggest = int(temp)
+
+        # get smallest
+        small = s[0]
+        temp = ""
+        for l in s:
+            if l == small:
+                temp += "0"
+            else:
+                temp += l
+        smallest = int(temp)
+
+        return biggest - smallest
